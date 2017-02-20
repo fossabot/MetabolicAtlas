bb7edb6783229e3db1119f19c28d325d5a3cae5f
Shan Huang <shan@chalmers.se>
2017-02-20 11:51:56 +0100

    Add more requests

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -9,4 +9,10 @@ urlpatterns = [
     url(r'^reactions/?$', views.reaction_list),
     url(r'^reactions/(?P<id>[^/]+)/?$', views.get_reaction),
     url(r'^reactions/(?P<id>[^/]+)/reactants/?$', views.reaction_reactant_list),
+    url(r'^reactions/(?P<reaction_id>[^/]+)/reactants/(?P<reactant_id>[^/]+)/?$', views.get_reaction_reactant),
+    url(r'^reactions/(?P<id>[^/]+)/products/?$', views.reaction_product_list),
+    url(r'^reactions/(?P<reaction_id>[^/]+)/products/(?P<product_id>[^/]+)/?$', views.get_reaction_product),
+    url(r'^reactions/(?P<id>[^/]+)/modifiers/?$', views.reaction_modifier_list),
+    url(r'^reactions/(?P<reaction_id>[^/]+)/modifiers/(?P<modifier_id>[^/]+)/?$', views.get_reaction_modifier),
+    url(r'^reaction_components/?$', views.component_list),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.db.models import Q
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view
@@ -71,4 +72,74 @@ def reaction_reactant_list(request, id):
     serializer = ReactionComponentSerializer(reaction.reactants, many=True)
     return JSONResponse(serializer.data)
 
+@api_view()
+def get_reaction_reactant(request, reaction_id, reactant_id):
+    try:
+        reaction = Reaction.objects.get(id=reaction_id)
+        reactant = reaction.reactants.get(id=reactant_id)
+    except Reaction.DoesNotExist:
+        return HttpResponse(status=404)
+
+    serializer = ReactionComponentSerializer(reactant)
+    return JSONResponse(serializer.data)
+
+@api_view()
+def reaction_product_list(request, id):
+    try:
+        reaction = Reaction.objects.get(id=id)
+    except Reaction.DoesNotExist:
+        return HttpResponse(status=404)
+
+    serializer = ReactionComponentSerializer(reaction.products, many=True)
+    return JSONResponse(serializer.data)
+
+@api_view()
+def get_reaction_product(request, reaction_id, product_id):
+    try:
+        reaction = Reaction.objects.get(id=reaction_id)
+        product = reaction.products.get(id=product_id)
+    except Reaction.DoesNotExist:
+        return HttpResponse(status=404)
+
+    serializer = ReactionComponentSerializer(product)
+    return JSONResponse(serializer.data)
+
+@api_view()
+def reaction_modifier_list(request, id):
+    try:
+        reaction = Reaction.objects.get(id=id)
+    except Reaction.DoesNotExist:
+        return HttpResponse(status=404)
+
+    serializer = ReactionComponentSerializer(reaction.modifiers, many=True)
+    return JSONResponse(serializer.data)
+
+@api_view()
+def get_reaction_modifier(request, reaction_id, modifier_id):
+    try:
+        reaction = Reaction.objects.get(id=reaction_id)
+        modifier = reaction.modifiers.get(id=modifier_id)
+    except Reaction.DoesNotExist:
+        return HttpResponse(status=404)
+
+    serializer = ReactionComponentSerializer(modifier)
+    return JSONResponse(serializer.data)
+
+@api_view()
+def component_list(request):
+    limit = int(request.query_params.get('limit', 20))
+    offset = int(request.query_params.get('offset', 0))
+
+    name = request.query_params.get('name', None)
+    if name:
+        components = ReactionComponent.objects.filter(
+                Q(id__icontains=name) | 
+                Q(long_name__icontains=name) | 
+                Q(short_name__icontains=name)
+            )[offset:(offset+limit)]
+    else:
+        components = ReactionComponent.objects.all()[offset:(offset+limit)]
+
+    serializer = ReactionComponentSerializer(components, many=True)
+    return JSONResponse(serializer.data)
 
