42b9b05f559c9768a8edbed0dc336c24f9da9d62
Shan Huang <shan@chalmers.se>
2017-04-19 16:36:35 +0200

    Fix image loader and use smaller sample image

--- frontend/build/webpack.base.conf.js
+++ frontend/build/webpack.base.conf.js
@@ -72,7 +72,6 @@ module.exports = {
         test: /\.(png|jpe?g|gif)(\?.*)?$/,
         loader: 'url',
         query: {
-          limit: 10000,
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
         }
       },
