abc7654ffa103701aab627342eb140f94263a30e
Shan Huang <shan@chalmers.se>
2017-04-19 11:24:00 +0200

    Add placeholder for metabolic network image

---
+++ frontend/src/components/MetabolicNetwork.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="metabolic-network">
+    <div class="container">
+      <img src="../assets/FILENAME.png">
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'metabolic-network',
+};
+</script>
+
+<style lang="scss">
+</style>
