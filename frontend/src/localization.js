5ee20770fe312197520c28fd1e3332e1ce17b33b
Shan Huang <shan@chalmers.se>
2017-04-25 13:33:13 +0200

    NGVFH-59 Add localization library

---
+++ frontend/src/localization.js
@@ -0,0 +1,5 @@
+export default {
+  en: {
+    footerText: 'Chalmers University of Technology | Department of Biology and Biological Engineering | <a href="http://sysbio.se" target="blank">Sys<sup>2</sup>Bio</a>',
+  },
+};
