84d8f26b00c84b5db55ac6951c3eeb6f5bc37a08
Lena Hansson <halena@chalmers.se>
2017-04-27 14:17:59 +0200

    added a switch to test if a file was supplied on the command line

--- image_helpers/analyse.pl
+++ image_helpers/analyse.pl
@@ -2,16 +2,20 @@ use strict;
 
 my $baseDir="/Users/halena/Documents/Sys2Bio/hma-prototype/frontend/src/assets/maps/";
 
-readFile("L", $baseDir."compartment_level/lysosome.svg");
-readFile("P", $baseDir."compartment_level/perixome.svg");
-readFile("ER", $baseDir."compartment_level/ER.svg");
-readFile("Golgi", $baseDir."compartment_level/golgi.svg");
-readFile("C1", $baseDir."compartment_level/cytosol_1.svg");
-readFile("C2", $baseDir."compartment_level/cytosol_2.svg");
-readFile("C3", $baseDir."compartment_level/cytosol_3.svg");
-readFile("C4", $baseDir."compartment_level/cytosol_4.svg");
-readFile("C5", $baseDir."compartment_level/cytosol_5.svg");
-readFile("C6", $baseDir."compartment_level/cytosol_6.svg");
+if($ARGV[0]){
+  readFile("input file", $baseDir."compartment_level/lysosome.svg");
+}else{
+  readFile("L", $baseDir."compartment_level/lysosome.svg");
+  readFile("P", $baseDir."compartment_level/perixome.svg");
+  readFile("ER", $baseDir."compartment_level/ER.svg");
+  readFile("Golgi", $baseDir."compartment_level/golgi.svg");
+  readFile("C1", $baseDir."compartment_level/cytosol_1.svg");
+  readFile("C2", $baseDir."compartment_level/cytosol_2.svg");
+  readFile("C3", $baseDir."compartment_level/cytosol_3.svg");
+  readFile("C4", $baseDir."compartment_level/cytosol_4.svg");
+  readFile("C5", $baseDir."compartment_level/cytosol_5.svg");
+  readFile("C6", $baseDir."compartment_level/cytosol_6.svg");
+}
 
 
 sub readFile{
