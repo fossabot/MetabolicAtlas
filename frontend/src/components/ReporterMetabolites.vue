8a3f6160e2a277a53618679cd4cb23ee396ef984
pecholleyc <cholley@chalmers.se>
2017-07-12 11:59:55 +0200

    add view to get reaction component ids

--- frontend/src/components/ReporterMetabolites.vue
+++ frontend/src/components/ReporterMetabolites.vue
@@ -1,21 +1,21 @@
 <template>
   <div>
-    <div class="column is-half">
-      <div class="field">
-        <div class="">
-          <label class="label">IDs: </label>
+    <div class="columns">
+      <div class="column is-half">
+        <div class="field">
+          <div class="">
+            <label class="label">IDs: </label>
+          </div>
+          <p class="control">
+            <textarea class="textarea" ref="textarea" placeholder="udp, h2o2, sam, m_m01784n"></textarea>
+          </p>
         </div>
-        <p class="control">
-          <textarea class="textarea"></textarea>
-        </p>
+      </div>
+      <div class="column">
+        <button class="button is-primary" @click="highLightElements">Highlight</button>
       </div>
     </div>
-    <div>
-      <button class="button is-primary">View</button>
-    </div>
-    <img src="assets/svg/nucleus_no_min.svg">
-    <img src="../assets/whole_metabolic_network.png" width="200px">
-    <div id="svg-wrapper" v-html="svgContent" style="width:600px; height:600px; border: 2px solid black">
+    <div id="svg-wrapper" v-html="svgContent" style="width:1200px; height:1200px; border: 2px solid black">
       <object type="image/svg+xml" data="../assets/svg/nucleus_no_min.svg">
         Your browser does not support SVG
       </object>
@@ -23,56 +23,76 @@
         Your browser does not support SVG
       </object>
     </div>
-    <svg id="svg" style="width:100px; height:100px; border: 2px solid black"></svg>
   </div>
 </template>
 
 <script>
 
 import { default as snap } from 'snapsvg';
+import axios from 'axios';
 
 export default {
   name: 'reporter-metabolites',
   data() {
     return {
       svgContent: '',
+      compartmentID: 8, // TODO make it dynamic
+      HLColor: '#22FFFF',
     };
   },
   mounted() {
-    // Snap demo
-    const s = snap('#svg');
-    // Lets create big circle in the middle:
-    const bigCircle = s.circle(150, 150, 100);
-    // By default its black, lets change its attributes
-    bigCircle.attr({
-      fill: '#bada55',
-      stroke: '#000',
-      strokeWidth: 5,
-    });
-    console.log('Snap demo output:', bigCircle);
-
     /* eslint-disable global-require */
-    const svgTest2 = require('assets/svg/nucleus_no_min.svg2');
-
-    this.svgContent = svgTest2;
-
-    console.log('start scaling');
-    s.selectAll('path') // or whatever ID it has, or give it one
-    .attr({ width: '100%', height: '100%', viewBox: '0 0 600 600' });
-
-    s.selectAll('g') // or whatever ID it has, or give it one
-    .attr({ width: '100%', height: '100%', viewBox: '0 0 600 600' });
+    // this.svgContent = require('assets/svg/ER.id_added.svg2');
+    this.svgContent = require('assets/svg/nucleus_no_min.svg2');
 
     // Example for modifying network SVG
     setTimeout(() => {
-      const s2 = snap('#svg-wrapper svg');
-      s2.select('g').attr({
-        fill: 'grey',
-      });
+      const s = snap('#svg-wrapper svg');
+      s.attr({ width: '1200px' });
+      s.attr({ height: '1200px' });
+
+      // console.log(s.select('path').transform());
+      // console.log(s.selectAll('path'));
+
+      /* s.selectAll('path') // or whatever ID it has, or give it one
+      .attr({ width: '100%', height: '100%', viewBox: '0 0 600 600' });
+
+      s.selectAll('g') // or whatever ID it has, or give it one
+      .attr({ width: '100%', height: '100%', viewBox: '0 0 600 600' }); */
     }, 0);
   },
   methods: {
+    highLightElements() {
+      const termsString = this.$refs.textarea.value;
+
+      const arrayTerms = termsString.trim().split(',');
+      const filterArray = [];
+      for (let i = 0; i < arrayTerms.length; i += 1) {
+        const trimTerm = arrayTerms[i].trim();
+        if (trimTerm.length !== 0) {
+          filterArray.push(trimTerm);
+        }
+      }
+      this.hlReactionComponentIDs(filterArray);
+    },
+    hlReactionComponentIDs(array) {
+      // get the correct IDs from the backend
+      axios.get(`convert_to_reaction_component_ids/${this.compartmentID}/${array.join(',')}`)
+      .then((response) => {
+        const result = response.data;
+        const s = snap('#svg-wrapper svg');
+        for (let i = 0; i < result.length; i += 1) {
+          const id = result[i].trim();
+          const elms = s.selectAll(`.${id}`);  // rcID should be assign to class attribut
+          for (let j = 0; j < elms.length; j += 1) {
+            elms[j].attr({ fill: this.HLColor });
+          }
+        }
+      })
+      .catch(() => {});
+    },
   },
+
 };
 </script>
 
