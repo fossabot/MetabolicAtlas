c3f27104e94ec472790eccbe6e932eaefbcfd863
pecholleyc <cholley@chalmers.se>
2017-08-18 19:17:11 +0200

    fix the name of the component in the left sidebar
    the name was not visible if the user do not select a node

--- frontend/src/components/ClosestInteractionPartners.vue
+++ frontend/src/components/ClosestInteractionPartners.vue
@@ -359,7 +359,6 @@ export default {
 
           [this.rawElms, this.rawRels] = transform(component, component.id, reactions);
           this.selectedElm = this.rawElms[component.id];
-          // this.rawElms = this.loadHPAData(this.rawElms);
           this.selectedElm.name = this.componentName;
 
           this.expandedIds = [];
