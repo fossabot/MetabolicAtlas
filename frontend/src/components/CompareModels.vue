07243d36b58af8ea9ecead2509eb53c243213de3
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2017-08-29 15:58:28 +0200

    Prettier page, put almost everything into tables, added sections, titles, subtitles

--- frontend/src/components/CompareModels.vue
+++ frontend/src/components/CompareModels.vue
@@ -1,143 +1,138 @@
 <template>
   <div>
-    <div class="container">
-        <h2>Comparing two models</h2>
-        <table class="table is-narrow is-bordered">
-          <tbody>
-            <tr v-for="model in comparison.Models">
-              <td>{{ model.ModelId }}</td>
-              <td>{{ model.ModelName }}</td>
-              <td>{{ model.TotalReactions }} reactions</td>
-              
-            </tr>
-          </tbody>
-        </table>
-      </br>
-        <h2>Differences found:</h2>
-        {{ comparison.Summary.SharedReactions }} complete shared reactions [{{ Math.round(comparison.Summary.SharedReactions / comparison.Models.A.TotalReactions * 100) }}% shared of As reaction set] [{{ Math.round(comparison.Summary.SharedReactions / comparison.Models.B.TotalReactions * 100) }}% shared of Bs reaction set]</br>
-        {{ comparison.Summary.ReactionsOnlyInA }} reactions only in A and {{ comparison.Summary.ReactionsOnlyInB }} reactions only in B</br>
-
-        <font color='orange'>FIXME where does this 611 come from?</font>
-        In addition another 611 reactions are the same, but different modifiers are 'indicated' in the two models.</br>
-      </br>
-
-<div class="columns">
-  <div class="column">
-    <table class="table is-narrow">
-      <thead>
-        <tr><th colspan="4">Most affected subsystems by missing reactions</th></tr>
-        <tr>
-          <th>Subsystem</th>
-          <th>reactions</th>
-          <th>A</th>
-          <th>B</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="subsys in topAffectedSysMis">
-          <td>{{ subsys.name }}</td>
-          <td>{{ subsys.totalMissing }}</td>
-          <td>{{ subsys.MissingReactionsFromA }}</td>
-          <td>{{ subsys.MissingReactionsFromB }}</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-  <div class="column">
-    <table class="table is-narrow">
-      <thead>
-        <tr><th colspan="2">Most affected subsystems by gene expression</th></tr>
-        <tr>
-          <th>Subsystem</th>
-          <th>reactions</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="subsys in topAffectedSysGenEx">
-          <td>{{ subsys.name }}</td>
-          <td>{{ subsys.DifferentModifiers }}</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-  <div class="column">
-    <table class="table is-narrow">
-      <thead>
-        <tr><th colspan="4">Most affected compartments by missing reactions</th></tr>
-        <tr>
-          <th>Subsystem</th>
-          <th>reactions</th>
-          <th>A</th>
-          <th>B</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="subsys in topAffectedCompMis">
-          <td>{{ subsys.name }}></td>
-          <td>{{ subsys.totalMissing }}</td>
-          <td>{{ subsys.MissingReactionsFromA }}</td>
-          <td>{{ subsys.MissingReactionsFromB }}</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-</div>
-<font color="orange">See more needs implementing and 
-Each of the names should be a link -> show on the SVG</font>
-</br>
-</br>
-      <font color='orange'>The below table should show all (?) affected reactions,
-      so it has to be possible to filter the table!</font>
-    <table class="table is-narrow">
-      <thead>
-        <tr><th colspan="6">All {{ amountReactions }} affected reactions</th></tr>
-        <tr>
-          <th>ID</th>
-          <th>Subsystem</th>
-          <th>Compartment</th>
-          <th>A</th>
-          <th>B</th>
-          <th>Show on map</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="rxn in comparison.AffectedReactions.VariousModifiers">
-          <td><a href="show on map">{{ rxn.ReactionId }}</a></td>
-          <td>{{ rxn.Subsystem }}</td>
-          <td>{{ rxn.Compartments.join(', ') }}</td>
-          <td>
-            <a href="" v-for="mod in rxn.ModifierDiferenceses.ModifiersInANotInB">
-              {{ mod }}
-            </a>
-          </td>
-          <td>
-            <a href="" v-for="mod in rxn.ModifierDiferenceses.ModifiersInBNotInA">
-              {{ mod }}
-            </a>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-<!--
-        <h2>The 3355 affected Reactions:</h2>
-        <table>
-          <tr><th>ReactionID</th><th>Subsystem</th><th>Compartment</th><th>A</th><th>B</th><th>Show on map</td></tr>
-          <tr><td>R_HMR_3905</td><td>Glycolysis / Gluconeogenesis</td><td>Cytosol</td>
-            <td><a href="">E_1357</a>, <a href="">E_1981</a>, <a href="">E_2141</a></td>
-            <td><a href="">E_1340</a>, <a href="">E_1871</a>, <a href="">E_1978</a>,
-              <a href="">E_2090</a>, <a href="">E_2109</a>, <a href="">E_2119</a>,
-              <a href="">E_2151</a>, <a href="">E_2158</a>, <a href="">E_2309</a></td>
-            <td><input type="radio"></tr>
-          <tr><td>R_HMR_8585</td><td>Starch and sucrose metabolism</td><td>Cytosol</td><td>Yes</td><td>No</td></tr>
-          <tr><td>R_HMR_8570</td><td>Starch and sucrose metabolism</td><td>Cytosol</td><td>No</td><td>Yes</td></tr>
-</tbody>
-        </table>
-        <font color='orange'>Clicking on the radio button should instantly load the corresponding.... WHAT NEEDS LOADING AND WHY A RADIO BUTTON AND NOT A LINK?</font>
-      </br>
-      <font color='orange'>Do we want to do some sort of GO overrepresentation analysis of the
-        enzymes that are found in A but not B (and vice verse)?</font>
--->
+    <section class="section">
+      <div class="container">
+          <h4 class="title is-4">Comparing two models</h4>
+          <h6 class="subtitle is-6">{{ comparison.Summary.SharedReactions }} complete shared reactions</h6>
+          <table class="table is-narrow is-bordered">
+            <thead>
+              <tr>
+                <th>Model</td>
+                <th>Description</td>
+                <th># of reactions</td>
+                <th>Shared reactions overlap</td>
+                <th>Exclusive reactions</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="model in comparison.Models">
+                <td>{{ model.ModelId }}</td>
+                <td>{{ model.ModelName }}</td>
+                <td>{{ model.TotalReactions }} reactions</td>
+                <td>{{ Math.round(comparison.Summary.SharedReactions / model.TotalReactions * 100) }}%</td>
+                <td>{{ comparison.Models.A.ModelId === model.ModelId ? comparison.Summary.ReactionsOnlyInA : comparison.Summary.ReactionsOnlyInB }} reactions</td>
+              </tr>
+            </tbody>
+          </table>
+          In addition another {{ amountRxnDifMod  }} reactions are the same, but different modifiers are 'indicated' in the two models.</br>
     </div>
+  </section>
+  
+    <section class="section">
+      <div class="container">
+  <h4 class="title is-4">Ranked affected parts</h4>
+  <div class="columns">
+    <div class="column">
+      <h6 class="subtitle is-6">Subsystems by missing reactions</h6>
+      <table class="table is-narrow">
+        <thead>
+          <tr>
+            <th>Subsystem</th>
+            <th>reactions</th>
+            <th>A</th>
+            <th>B</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="subsys in topAffectedSysMis">
+            <td>{{ subsys.name }}</td>
+            <td>{{ subsys.totalMissing }}</td>
+            <td>{{ subsys.MissingReactionsFromA }}</td>
+            <td>{{ subsys.MissingReactionsFromB }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="column">
+      <h6 class="subtitle is-6">Subsystems by gene expression</h6>
+      <table class="table is-narrow">
+        <thead>
+          <tr>
+            <th>Subsystem</th>
+            <th>reactions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="subsys in topAffectedSysGenEx">
+            <td>{{ subsys.name }}</td>
+            <td>{{ subsys.DifferentModifiers }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="column">
+      <h6 class="subtitle is-6">Compartments by missing reactions</h6>
+      <table class="table is-narrow">
+        <thead>
+          <tr>
+            <th>Compartment</th>
+            <th>reactions</th>
+            <th>A</th>
+            <th>B</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="subsys in topAffectedCompMis">
+            <td>{{ subsys.name }}</td>
+            <td>{{ subsys.totalMissing }}</td>
+            <td>{{ subsys.MissingReactionsFromA }}</td>
+            <td>{{ subsys.MissingReactionsFromB }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+  <font color="orange">See more needs implementing and 
+  Each of the names should be a link -> show on the SVG</font>
+  </br>
+  </br>
+        <font color='orange'>The below table should show all (?) affected reactions,
+        so it has to be possible to filter the table!</font>
+      <table class="table is-narrow">
+        <thead>
+          <tr><th colspan="6">All {{ amountReactions }} affected reactions</th></tr>
+          <tr>
+            <th>ID</th>
+            <th>Subsystem</th>
+            <th>Compartment</th>
+            <th>A</th>
+            <th>B</th>
+            <th>Draw</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="rxn in comparison.AffectedReactions.VariousModifiers">
+            <td>{{ rxn.ReactionId }}</td>
+            <td>{{ rxn.Subsystem }}</td>
+            <td>{{ rxn.Compartments.join(', ') }}</td>
+            <td>
+              <a href="" v-for="mod in rxn.ModifierDiferenceses.ModifiersInANotInB">
+                {{ mod }}
+              </a>
+            </td>
+            <td>
+              <a href="" v-for="mod in rxn.ModifierDiferenceses.ModifiersInBNotInA">
+                {{ mod }}
+              </a>
+            </td>
+            <td><input type="checkbox"></td>
+          </tr>
+        </tbody>
+      </table>
+        <font color='orange'>Do we want to do some sort of GO overrepresentation analysis of the
+          enzymes that are found in A but not B (and vice verse)?</font>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -197,9 +192,11 @@ export default {
       this.affectedCompartments.sort(comparer);
       return this.affectedCompartments.slice(0, 5);
     },
+    amountRxnDifMod() {
+      return this.comparison.AffectedReactions.VariousModifiers.length;
+    },
     amountReactions() {
-      return (this.comparison.AffectedReactions.VariousModifiers.length +
-              this.comparison.AffectedReactions.LostReactions.length);
+      return this.amountRxnDifMod + this.comparison.AffectedReactions.LostReactions.length;
     },
   },
   methods: {
