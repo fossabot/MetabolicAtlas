ed68191c17a7f6c8931cf9b59b6edcdbf49fb84b
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2017-08-29 13:21:29 +0200

    Created tables for compare models from example JSON data included in this commit

--- frontend/src/components/CompareModels.vue
+++ frontend/src/components/CompareModels.vue
@@ -1,57 +1,124 @@
 <template>
   <div>
     <div class="container">
-      <p>
         <h2>Comparing two models</h2>
-        <table border=1>
-          <tr><td width=25>A</td><td>INITModel: Automatically generated model for RNA - HEK 293</td><td>5274 reactions</td></tr>
-          <tr><td>B</td><td>Hepatocytes2322: Genome-scale metabolic model for hepatocytes, iHepatocytes2322</td><td>7930 reactions</td></tr>
+        <table class="table is-narrow is-bordered">
+          <tbody>
+            <tr v-for="model in comparison.Models">
+              <td>{{ model.ModelId }}</td>
+              <td>{{ model.ModelName }}</td>
+              <td>{{ model.TotalReactions }} reactions</td>
+              
+            </tr>
+          </tbody>
         </table>
-      </p>
       </br>
-      <p>
         <h2>Differences found:</h2>
-        4619 complete shared reactions [86% shared of As reaction set] [59% shared of Bs reaction set]</br>
-        44 reactions only in A and 2700 reactions only in B</br>
+        {{ comparison.Summary.SharedReactions }} complete shared reactions [{{ Math.round(comparison.Summary.SharedReactions / comparison.Models.A.TotalReactions * 100) }}% shared of As reaction set] [{{ Math.round(comparison.Summary.SharedReactions / comparison.Models.B.TotalReactions * 100) }}% shared of Bs reaction set]</br>
+        {{ comparison.Summary.ReactionsOnlyInA }} reactions only in A and {{ comparison.Summary.ReactionsOnlyInB }} reactions only in B</br>
+
+        <font color='orange'>FIXME where does this 611 come from?</font>
         In addition another 611 reactions are the same, but different modifiers are 'indicated' in the two models.</br>
-      </p>
       </br>
-      <p>
-        <table><tr><td>
-        <h2>The most affected subsystems - As based on missing reactions</h2>
-        <ul>
-          <li><a href=''>Transport, extracellular</a> (616 reactions: 608 from A and 8 from B)</li>
-          <li><a href=''>Transport, mitochondrial</a> (127 reactions: 127 from A)</li>
-          <li><a href=''>Bile acid biosynthesis </a>(102 reactions: 102 from A)</li>
-          <li><a href=''>Phenylalanine, tyrosine and tryptophan biosynthesis</a> (92 reactions: 92 from A)</li>
-          <li><a href=''>Carnitine shuttle (endoplasmic reticular)</a> (56 reactions: 56 from A)</li>
-          <li><a href="">...see more</a></li>
-        </ul>
-        </td><td>
-        <h2>The most affected subsystems - As based on gene expression</h2>
-        <ul>
-          <li><a href=''>Transport, extracellular</a> (887 reactions)</li>
-          <li><a href=''>Transport, mitochondrial</a> (146 reactions)</li>
-          <li><a href=''>Leukotriene metabolism</a> (136 reactions)</li>
-          <li><a href=''>Carnitine shuttle (mitochondrial)</a> (107 reactions)</li>
-          <li><a href=''>Glycerolipid metabolism</a> (74 reactions)</li>          
-          <li><a href="">...see more</a></li>
-        </ul>
-        </td></tr></table>
-        <font color='orange'>Each of the names should be a link -> show on the SVG</font>
-      </p>
-      <p>
-        <h2>The most affected compartments - As based on missing reactions</h2>
-        <ul>
-          <li>Cytosol (1692 reactions: 1676 from A and 16 from B)</li>
-          <li>Extracellular (372 reactions: 359 from A and 13 from B)</li>
-          <li><a href="">...see more</a></li>
-        </ul>
-      </p>
-      </br>
+
+<div class="columns">
+  <div class="column">
+    <table class="table is-narrow">
+      <thead>
+        <tr><th colspan="4">Most affected subsystems by missing reactions</th></tr>
+        <tr>
+          <th>Subsystem</th>
+          <th>reactions</th>
+          <th>A</th>
+          <th>B</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="subsys in topAffectedSysMis">
+          <td>{{ subsys.name }}</td>
+          <td>{{ subsys.totalMissing }}</td>
+          <td>{{ subsys.MissingReactionsFromA }}</td>
+          <td>{{ subsys.MissingReactionsFromB }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  <div class="column">
+    <table class="table is-narrow">
+      <thead>
+        <tr><th colspan="2">Most affected subsystems by gene expression</th></tr>
+        <tr>
+          <th>Subsystem</th>
+          <th>reactions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="subsys in topAffectedSysGenEx">
+          <td>{{ subsys.name }}</td>
+          <td>{{ subsys.DifferentModifiers }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  <div class="column">
+    <table class="table is-narrow">
+      <thead>
+        <tr><th colspan="4">Most affected compartments by missing reactions</th></tr>
+        <tr>
+          <th>Subsystem</th>
+          <th>reactions</th>
+          <th>A</th>
+          <th>B</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="subsys in topAffectedCompMis">
+          <td>{{ subsys.name }}></td>
+          <td>{{ subsys.totalMissing }}</td>
+          <td>{{ subsys.MissingReactionsFromA }}</td>
+          <td>{{ subsys.MissingReactionsFromB }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
+<font color="orange">See more needs implementing and 
+Each of the names should be a link -> show on the SVG</font>
+</br>
+</br>
       <font color='orange'>The below table should show all (?) affected reactions,
       so it has to be possible to filter the table!</font>
-      <p>
+    <table class="table is-narrow">
+      <thead>
+        <tr><th colspan="6">All {{ amountReactions }} affected reactions</th></tr>
+        <tr>
+          <th>ID</th>
+          <th>Subsystem</th>
+          <th>Compartment</th>
+          <th>A</th>
+          <th>B</th>
+          <th>Show on map</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="rxn in comparison.AffectedReactions.VariousModifiers">
+          <td><a href="show on map">{{ rxn.ReactionId }}</a></td>
+          <td>{{ rxn.Subsystem }}</td>
+          <td>{{ rxn.Compartments.join(', ') }}</td>
+          <td>
+            <a href="" v-for="mod in rxn.ModifierDiferenceses.ModifiersInANotInB">
+              {{ mod }}
+            </a>
+          </td>
+          <td>
+            <a href="" v-for="mod in rxn.ModifierDiferenceses.ModifiersInBNotInA">
+              {{ mod }}
+            </a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+<!--
         <h2>The 3355 affected Reactions:</h2>
         <table>
           <tr><th>ReactionID</th><th>Subsystem</th><th>Compartment</th><th>A</th><th>B</th><th>Show on map</td></tr>
@@ -63,20 +130,84 @@
             <td><input type="radio"></tr>
           <tr><td>R_HMR_8585</td><td>Starch and sucrose metabolism</td><td>Cytosol</td><td>Yes</td><td>No</td></tr>
           <tr><td>R_HMR_8570</td><td>Starch and sucrose metabolism</td><td>Cytosol</td><td>No</td><td>Yes</td></tr>
+</tbody>
         </table>
-        <font color='orange'>Clicking on the radio button should instantly load the corresponding....</font>
-      </p>
+        <font color='orange'>Clicking on the radio button should instantly load the corresponding.... WHAT NEEDS LOADING AND WHY A RADIO BUTTON AND NOT A LINK?</font>
       </br>
       <font color='orange'>Do we want to do some sort of GO overrepresentation analysis of the
         enzymes that are found in A but not B (and vice verse)?</font>
+-->
     </div>
   </div>
 </template>
 
 <script>
+import { default as compareResponse } from '../helpers/compareresp';
 
 export default {
-  name: 'about',
+  name: 'compare-models',
+  data() {
+    return {
+      loading: true,
+      comparison: {},
+    };
+  },
+  beforeMount() {
+    this.setup();
+  },
+  computed: {
+    affectedSubsystems() {
+      const affectedSubsystems = [];
+      for (const subsys of Object.entries(this.comparison.Summary.AffectedSubsystems)) {
+        const entry = Object.assign({}, subsys[1]);
+        entry.name = subsys[0];
+        entry.totalMissing = entry.MissingReactionsFromA + entry.MissingReactionsFromB;
+        affectedSubsystems.push(entry);
+      }
+      return affectedSubsystems;
+    },
+    affectedCompartments() {
+      const affectedCompartments = [];
+      for (const comp of Object.entries(this.comparison.Summary.AffectedCompartments)) {
+        const entry = Object.assign({}, comp[1]);
+        entry.name = comp[0];
+        entry.totalMissing = entry.MissingReactionsFromA + entry.MissingReactionsFromB;
+        affectedCompartments.push(entry);
+      }
+      return affectedCompartments;
+    },
+    topAffectedSysMis() {
+      function comparer(a, b) {
+        return b.totalMissing - a.totalMissing;
+      }
+      this.affectedSubsystems.sort(comparer);
+      return this.affectedSubsystems.slice(0, 5);
+    },
+    topAffectedSysGenEx() {
+      function comparer(a, b) {
+        return b.DifferentModifiers - a.DifferentModifiers;
+      }
+      this.affectedSubsystems.sort(comparer);
+      return this.affectedSubsystems.slice(0, 5);
+    },
+    topAffectedCompMis() {
+      function comparer(a, b) {
+        return b.totalMissing - a.totalMissing;
+      }
+      this.affectedCompartments.sort(comparer);
+      return this.affectedCompartments.slice(0, 5);
+    },
+    amountReactions() {
+      return (this.comparison.AffectedReactions.VariousModifiers.length +
+              this.comparison.AffectedReactions.LostReactions.length);
+    },
+  },
+  methods: {
+    setup() {
+      // FIXME now loads a JSON file
+      this.comparison = JSON.parse(compareResponse());
+    },
+  },
 };
 </script>
 
