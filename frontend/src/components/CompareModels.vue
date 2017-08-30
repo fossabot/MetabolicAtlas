92aad76bdcd218f06b0ec9c8a8cf3acc344e42e0
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2017-08-30 13:49:08 +0200

    Started with filtering of table

--- frontend/src/components/CompareModels.vue
+++ frontend/src/components/CompareModels.vue
@@ -102,20 +102,62 @@
         so it has to be possible to filter the table!</font>
     <section class="section">
       <div class="container">
-        <h4 class="title is-4">All {{ amountReactions }} affected reactions</h4>
+        <h4 class="title is-4" v-if="showAffected == 'all'">{{ filterableReactions.length }} reactions</h4>
+        <h4 class="title is-4" v-if="showAffected == 'lost'">{{ filterableReactions.length }} non-shared (lost) reactions</h4>
+        <h4 class="title is-4" v-if="showAffected == 'various'">{{ filterableReactions.length }} shared reactions (VariousModifiers)</h4>
+        <h6 class="subtitle is-6">{{ filteredAffectedReactions.length }} filtered, {{ showAffectedReactions.length }} shown</h6>
+              <div class="select">
+                <select v-model="showAffected" class="select">
+                  <option value="all">Show all reactions</option>
+                  <option value="lost">Show lost reactions</option>
+                  <option value="various">Show various modifiers</option>
+                </select>
+              </div>
         <table class="table is-narrow">
           <thead>
+            <tr>
+              <th>
+                <input class="input" v-model="filters.modelID" type="text" placeholder="Filter...">
+              </th>
+              <th>
+                <input class="input" v-model="filters.subsys" type="text" placeholder="Filter...">
+              </th>
+              <th>
+                <input class="input" v-model="filters.compartment" type="text" placeholder="Filter...">
+              </th>
+              <th>
+                <input class="input" v-if="showAffected !== 'lost'" v-model="filters.filterA" type="text" placeholder="Filter...">
+                <div class="select">
+                  <select class="select" v-model="filters.tfA" v-if="showAffected !== 'various'">
+                    <option value="all">Show all</option>
+                    <option value="true">Only true</option>
+                    <option value="false">Only false</option>
+                  </select>
+                </div>
+              </th>
+              <th>
+                <input class="input" v-if="showAffected !== 'lost'" v-model="filters.filterB" type="text" placeholder="Filter...">
+                <div class="select">
+                  <select class="select" v-model="filters.tfB" v-if="showAffected !== 'various'">
+                    <option value="all">Show all</option>
+                    <option value="true">Only true</option>
+                    <option value="false">Only false</option>
+                  </select>
+                </div>
+              </th>
+              <th><button class="button" v-on:click="filterAffectedReactions(filters)">Filter</button></th>
+            </tr>
             <tr>
               <th>ID</th>
               <th>Subsystem</th>
               <th>Compartment</th>
               <th>A</th>
               <th>B</th>
-              <th>Draw</th>
+              <th><button class="button">Draw</button></th>
             </tr>
           </thead>
           <tbody>
-            <tr v-for="rxn in allAffectedReactions">
+            <tr v-for="rxn in showAffectedReactions">
               <td>{{ rxn.ReactionId }}</td>
               <td>{{ rxn.Subsystem }}</td>
               <td>{{ rxn.Compartments.join(', ') }}</td>
@@ -158,11 +200,26 @@ export default {
   data() {
     return {
       loading: true,
+      amountRxnShowing: 3,
+      showAffected: 'lost',
       comparison: {},
+      filters: {
+        modelID: '',
+        subsys: '',
+        compartment: '',
+        tfA: 'all',
+        tfB: 'all',
+        filterA: '',
+        filterB: '',
+      },
+      filteredAffectedReactions: [],
     };
   },
   beforeMount() {
     this.setup();
+  },
+  mounted() {
+    this.filteredAffectedReactions = this.filterableReactions;
   },
   computed: {
     affectedSubsystems() {
@@ -185,9 +242,23 @@ export default {
       }
       return affectedCompartments;
     },
-    allAffectedReactions() {
-      return this.comparison.AffectedReactions.VariousModifiers.concat(
-        this.comparison.AffectedReactions.LostReactions);
+    filterableReactions() {
+      console.log('big filter');
+      let filterable = [];
+      if (this.showAffected === 'lost') {
+        filterable = this.comparison.AffectedReactions.LostReactions;
+      } else if (this.showAffected === 'various') {
+        filterable = this.comparison.AffectedReactions.VariousModifiers;
+      } else {
+        filterable = this.comparison.AffectedReactions.VariousModifiers.concat(
+                     this.comparison.AffectedReactions.LostReactions);
+      }
+      this.filteredAffectedReactions = filterable;
+      return filterable;
+    },
+    showAffectedReactions() {
+      // possibly slice this list if too many
+      return this.filteredAffectedReactions.slice(0, 100);
     },
     topAffectedSysMis() {
       function comparer(a, b) {
@@ -213,14 +284,40 @@ export default {
     amountRxnDifMod() {
       return this.comparison.AffectedReactions.VariousModifiers.length;
     },
-    amountReactions() {
-      return this.amountRxnDifMod + this.comparison.AffectedReactions.LostReactions.length;
-    },
   },
   methods: {
     setup() {
       // FIXME now loads a JSON file
       this.comparison = JSON.parse(compareResponse());
+    },
+    filterAffectedReactions(filters) {
+      console.log('small filter');
+      const filteredReactions = [];
+      for (const rxn of this.filterableReactions) {
+        let show = true;
+        if (show && rxn.FoundInA !== 'all' && rxn.ModifierDiferenceses === null &&
+            rxn.FoundInA !== filters.tfA) {
+          show = false;
+        }
+        if (show && rxn.FoundInB !== 'all' && rxn.ModifierDiferenceses === null &&
+            rxn.FoundInB !== filters.tfB) {
+          show = false;
+        }
+        if (show && filters.filterA &&
+            rxn.ModifierDiferenceses !== null &&
+            rxn.ModifierDiferenceses.ModifiersInANotInB.join().indexOf(filters.filterA) === -1) {
+          show = false;
+        }
+        if (show && filters.filterB &&
+            rxn.ModifierDiferenceses !== null &&
+            rxn.ModifierDiferenceses.ModifiersInBNotInA.join().indexOf(filters.filterB) === -1) {
+          show = false;
+        }
+        if (show) {
+          filteredReactions.push(rxn);
+        }
+      }
+      this.filteredAffectedReactions = filteredReactions;
     },
   },
 };
