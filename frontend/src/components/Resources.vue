d552a9ada57a99be11bcbaf9ad81a3dd7e8b2dc6
Lena Hansson <halena@chalmers.se>
2017-09-06 11:06:50 +0200

    added some information to the Resources page after having gotten an email from Avlant

--- frontend/src/components/Resources.vue
+++ frontend/src/components/Resources.vue
@@ -7,7 +7,7 @@
           <div class="columns">
             <div class="column is-1">
               <a v-if="tool_data.img" :href="tool_data.link" target="_blank">
-                <img :src="tool_data.img" width="150">
+                <img :src="tool_data.img" width="160">
               </a>
               <a v-else :href="tool_data.link" target="_blank">
                 <div class="name">
@@ -18,7 +18,7 @@
             <div class="column">
               <div class="dsc">
                 <span>
-                {{ tool_data.description }}
+                  <div v-html="tool_data.description"></div>
                 </span>
               </div>
             </div>
@@ -75,20 +75,20 @@ export default {
       tools: [
         { name: 'RAVEN',
           link: 'https://github.com/SysBioChalmers/RAVEN/',
-          img: '',
-          description: 'RAVEN (Reconstruction, Analysis and Visualization of Metabolic Networks) Toolbox is a software suite that allows for semi-automated reconstruction of genome-scale models. It makes use of published models and/or the KEGG database, coupled with extensive gap-filling and quality control features.' },
+          img: 'http://biomet-toolbox.chalmers.se/img/ravenLogo.png',
+          description: '<b>RAVEN (Reconstruction, Analysis and Visualization of Metabolic Networks) Toolbox is a software suite that allows for semi-automated reconstruction of genome-scale models</b>. It makes use of published models and/or the KEGG database, coupled with extensive gap-filling and quality control features. <br>For more information please refer to <a href="http://journals.plos.org/ploscompbiol/article?id=10.1371/journal.pcbi.1002980">Agren, R., Liu, L., Shoaie, S., Vongsangnak, W., Nookaew, I., Nielsen, J. (2013). <i>The RAVEN Toolbox and Its Use for Generating a Genome-scale Metabolic Model for Penicillium chrysogenum</i> PLOS Computational Biology</a>.' },
         { name: 'Gecko',
           link: 'https://github.com/SysBioChalmers/GECKO',
           img: 'https://github.com/SysBioChalmers/GECKO/raw/master/GECKO.png?raw=true',
-          description: 'The GECKO toolbox is a Matlab/Python package for enhancing a Genome-scale model to account for Enzyme Constraints, using Kinetics and Omics.' },
+          description: '<b>The GECKO toolbox is a Matlab/Python package for enhancing a Genome-scale model to account for Enzyme Constraints, using Kinetics and Omics</b>.' },
         { name: 'STIG-met',
           link: 'https://github.com/SysBioChalmers/STIG-met',
           img: '',
-          description: 'Simulation Toolbox for Infant Growth with focus on Metabolism.' },
+          description: '<b>Simulation Toolbox for Infant Growth with focus on Metabolism (STIG-met) is an integrated platform for simulation of human growth</b>. <br> We combine the experience from traditional growth models with GEMs to provide predictions of metabolic fluxes with enzyme level resolution on a day-to-day basis.<br>For using STIG-met, please refer to <a href="http://www.nature.com/articles/s41540-017-0004-5">Nilsson, A., Mardinoglu, A., and Nielsen, J. (2017).<i>Predicting growth of the healthy infant using a genome scale metabolic model.</i>Npj Systems Biology and Applications, 3(1), 3</a>' },
         { name: 'Kiwi',
           link: 'https://github.com/SysBioChalmers/Kiwi',
           img: 'https://pythonhosted.org/KiwiDist/_images/kiwi_logo.png',
-          description: 'The Kiwi module combines geneset analyses with biological networks to visualize the interactions between genesets that are significant in a given biological system.' },
+          description: '<b>The Kiwi module combines geneset analyses with biological networks to visualize the interactions between genesets that are significant in a given biological system</b> such that the inherent connectivity between gene-sets becomes apparent, by visualize whether these entities or processes are isolated or connected by means of their biological interaction. <br>For more information please refer to <a href="https://bmcbioinformatics.biomedcentral.com/articles/10.1186/s12859-014-0408-9">V&auml;remo, L., Gatto, F., Nielsen, J. (2014). <i>Kiwi: a tool for integration and visualization of network topology and gene-set analysis</i> BMC Bioinformatics</a>.' },
       ],
       databases: [
         { name: 'YSeq Browser',
@@ -136,7 +136,7 @@ export default {
   }
 
   .dsc, .name {
-    height: 75px;
+    height: 100px;
     line-height: 75px;
 
     span {
