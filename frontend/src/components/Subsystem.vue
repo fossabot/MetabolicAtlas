2fff14a1c2b8dfbc02b53f049e731d5344db8b41
pecholleyc <cholley@chalmers.se>
2017-10-05 19:12:36 +0200

    add loader

--- frontend/src/components/Subsystem.vue
+++ frontend/src/components/Subsystem.vue
@@ -4,42 +4,48 @@
       {{ errorMessage }}
     </div>
   </div>
-  <div v-else class="subsystem-table">
-    <table v-if="info && Object.keys(info).length != 0" class="table main-table">
-      <tr class="m-row" v-for="el in mainTableKey">
-        <td v-if="el.display" class="td-key">{{ el.display }}</td>
-        <td v-else class="td-key">{{ reformatKey(el.name) }}</td>
-        <td v-if="info[el.name]">
-          <span v-if="el.modifier" v-html="el.modifier(info[el.name])">
-          </span>
-          <span v-else>
-            {{ info[el.name] }}
-          </span>
-        </td>
-        <td v-else> - </td>
-      </tr>
-      <tr>
-        <td class="td-key">Compartments</td>
-        <td>{{ compartmentListHtml }}</td>
-      </tr>
-      <tr>
-        <td class="td-key">Metabolites</td>
-        <td v-html="metabolitesListHtml"></td>
-      </tr>
-      <tr>
-        <td class="td-key">Enzymes</td>
-        <td v-html="enzymesListHtml"></td>
-      </tr>
-    </table>
-    <h3 class="title is-3">Reactions</h3>
-    <reaction-table :reactions="reactions" :showSubsystem="false"></reaction-table>
-    </table>
+  <div v-else>
+    <loader v-show="loading"></loader>
+    <div v-show="!loading">
+      <div class="subsystem-table">
+        <table v-if="info && Object.keys(info).length != 0" class="table main-table">
+          <tr class="m-row" v-for="el in mainTableKey">
+            <td v-if="el.display" class="td-key">{{ el.display }}</td>
+            <td v-else class="td-key">{{ reformatKey(el.name) }}</td>
+            <td v-if="info[el.name]">
+              <span v-if="el.modifier" v-html="el.modifier(info[el.name])">
+              </span>
+              <span v-else>
+                {{ info[el.name] }}
+              </span>
+            </td>
+            <td v-else> - </td>
+          </tr>
+          <tr>
+            <td class="td-key">Compartments</td>
+            <td>{{ compartmentListHtml }}</td>
+          </tr>
+          <tr>
+            <td class="td-key">Metabolites</td>
+            <td v-html="metabolitesListHtml"></td>
+          </tr>
+          <tr>
+            <td class="td-key">Enzymes</td>
+            <td v-html="enzymesListHtml"></td>
+          </tr>
+        </table>
+        <h3 class="title is-3">Reactions</h3>
+        <reaction-table :reactions="reactions" :showSubsystem="false"></reaction-table>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
 import $ from 'jquery';
+import Loader from 'components/Loader';
 import ReactionTable from 'components/ReactionTable';
 import { default as EventBus } from '../event-bus';
 
@@ -47,10 +53,12 @@ export default {
   name: 'subsystem',
   components: {
     ReactionTable,
+    Loader,
   },
   data() {
     return {
       sId: this.$route.query.id,
+      loading: false,
       info: {},
       metabolites: [],
       enzymes: [],
@@ -103,12 +111,14 @@ export default {
       this.load();
     },
     load() {
+      this.loading = true;
       axios.get(`showsubsystem/${this.sId}/`)
       .then((response) => {
         this.info = response.data.subsystemAnnotations;
         this.metabolites = response.data.metabolites;
         this.enzymes = response.data.enzymes;
         this.reactions = response.data.reactions;
+        this.loading = false;
       })
       .catch(() => {
         this.errorMessage = this.$t('notFoundError');
