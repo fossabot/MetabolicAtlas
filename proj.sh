5d68f9fc57bfd1728e608ddffb1e71b914808959
pecholleyc <cholley@chalmers.se>
2018-02-19 13:43:55 +0100

    change files based on sysomics setup
    additionnal changes might be requiered

--- proj.sh
+++ proj.sh
@@ -33,15 +33,6 @@ function create-su {
     docker exec -it metabolicatlas_backend_1 python manage.py createsuperuser
 }
 
-function build-production {
-    docker exec metabolicatlas_frontend_1 npm run build
-    sudo rm -rf nginx/static
-    sudo mkdir nginx/static
-    sudo cp -r frontend/dist/* nginx/
-    sudo mv nginx/index.html nginx/static/
-    sudo cp -r backend/static/ nginx/static/
-    sudo rm -rf frontend/dist
-}
 
 echo -e "
 
@@ -55,6 +46,5 @@ Available commands:
 \tdb-make-migrations
 \tdb-migrate
 \tcreate-su
-\tbuild-production
 
 "
