7441e4da3f2fe60cf2bd91a264e20677483e4051
pecholleyc <cholley@chalmers.se>
2018-06-03 19:24:40 +0200

    refactor: fix the interaction partner data (NGVFH-352)
    - update cytoscape
    - add reaction/compartment/subsystem data in each node, add reaction data for each link, and side <select> (highlight feature NGVFH-353)
    - change graph layout: switch to 'cola' for small network (NGVFH-319)
    - remove unused  'cose-bilkent' package
    - clean the sidebar
    - move HPA RNA levels on the side of the network
    - add padding to fit() function (NGVFH-262)
    - add mid-arrow for links reactant/product-enzyme (NGVFH-46)
    - count nodes not anymore reactions, max nodes is set to 100 (NGVFH-213)

--- frontend/src/data-mappers/hmr-closest-interaction-partners.js
+++ frontend/src/data-mappers/hmr-closest-interaction-partners.js
@@ -1,39 +1,34 @@
 // import { default as getLink } from '../helpers/component-link';
+import Vue from 'vue';
 
-export default function (c, reactions) {
-  const elms = {};
-  const rels = {};
+export default function (c, reactions, relms, rrels, rcomp, rsub) {
+  /* eslint-disable no-param-reassign */
+  const elms = relms || {};
+  const rels = rrels || {};
+  let compartmentSet = rcomp;
+  if (!compartmentSet) {
+    compartmentSet = new Set();
+  } else {
+    // rcomp is a list
+    compartmentSet = new Set(compartmentSet);
+  }
+  let subsystemSet = rsub;
+  if (!subsystemSet) {
+    subsystemSet = new Set();
+  } else {
+    // rsub is a list
+    subsystemSet = new Set(subsystemSet);
+  }
 
-  const component = {
-    id: c.id,
-    type: c.type,
-    name: c.gene_name || c.name || c.id,
-    formula: c.formula,
-    compartment: c.compartment,
-    // link: getLink(c),
-    // details: c.metabolite || c.enzyme,
-  };
-
-  elms[component.id] = component;
+  let order = 1;
+  if (relms || rrels) {
+    order = 2;
+  }
 
   for (const r of reactions) {
-    const mods = {};
-    for (const m of r.modifiers) {
-      const modifier = {
-        id: m.id,
-        type: 'enzyme',
-        name: m.gene_name || m.id,
-        formula: m.formula,
-        compartment: m.compartment,
-        reaction: r.id,
-        // link: getLink(m),
-        // details: m.metabolite || m.enzyme,
-      };
-
-      mods[modifier.id] = modifier;
-    }
-
     const mets = {};
+    const reactionComp = new Set();
+    subsystemSet.add(...r.subsystem);
     for (const m of [...r.products, ...r.reactants]) {
       const metabolite = {
         id: m.id,
@@ -41,68 +36,170 @@ export default function (c, reactions) {
         name: m.name,
         formula: m.formula,
         compartment: m.compartment,
-        reaction: r.id,
-        // link: getLink(m),
-        // details: m.metabolite || m.enzyme,
+        subsystem: new Set(),
+        reaction: new Set([r.id]),
       };
-
+      compartmentSet.add(m.compartment);
+      reactionComp.add(m.compartment);
+      metabolite.subsystem.add(...r.subsystem);
       mets[metabolite.id] = metabolite;
     }
 
+    const mods = {};
+    for (const m of r.modifiers) {
+      const modifier = {
+        id: m.id,
+        type: 'enzyme',
+        name: m.gene_name || m.id,
+        formula: m.formula,
+        compartment: {},
+        subsystem: new Set(),
+        reaction: new Set([r.id]),
+      };
+      if (reactionComp.size === 1) {
+        const compt = reactionComp.values().next().value;
+        modifier.compartment[compt] = 1;
+      } else {
+        // ambigous localization
+        reactionComp.forEach((e) => { modifier.compartment[e] = 0; });
+      }
+      modifier.subsystem.add(...r.subsystem);
+      mods[modifier.id] = modifier;
+    }
+
     for (const eid of Object.keys(mods)) {
-      if (!(eid in Object.keys(elms))) {
-        elms[eid] = mods[eid];
-      }
-    }
-
-    for (const eid of Object.keys(mets)) {
-      if (!(eid in Object.keys(elms))) {
-        elms[eid] = mets[eid];
-      }
-    }
-
-    for (const eidMo of Object.keys(mods)) {
-      for (const eidMe of Object.keys(mets)) {
-        const relation = {
-          id: `${eidMo}_${eidMe}`,
-          source: eidMo,
-          target: eidMe,
-        };
-
-        if (!(relation.id in Object.keys(rels))) {
-          const mod = mods[eidMo];
-          const met = mets[eidMe];
-
-          if (mod.reaction && mod.reaction === met.reaction) {
-            relation.reaction = mod.reaction;
-            rels[relation.id] = relation;
+      const e = mods[eid];
+      if (!(eid in elms)) {
+        Vue.set(elms, eid, e);
+      } else {
+        elms[eid].reaction.add(...e.reaction);
+        elms[eid].subsystem.add(...e.subsystem);
+        for (const cpt of Object.keys(e.compartment)) {
+          // new compartment or not ambigous localization for the existing enzyme
+          if (!(cpt in elms[eid].compartment) || e.compartment[cpt] === 1) {
+            elms[eid].compartment[cpt] = e.compartment[cpt];
           }
         }
       }
     }
 
-    for (const eidMo of Object.keys(mods)) {
-      const relation = {
-        id: `${eidMo}_${component.id}`,
-        target: eidMo,
-        source: component.id,
-        reaction: mods[eidMo].reaction,
-      };
+    for (const mid of Object.keys(mets)) {
+      const m = mets[mid];
+      if (!(mid in elms)) {
+        Vue.set(elms, mid, m);
+      } else {
+        elms[mid].reaction.add(...m.reaction);
+        elms[mid].subsystem.add(...m.subsystem);
+      }
+    }
 
-      rels[relation.id] = relation;
+    // add relations between mets-mods components of the current reaction
+    for (const eidMo of Object.keys(mods)) {
+      for (const eidMe of [...r.reactants]) {
+        const relID = `${eidMo}_${eidMe.id}`;
+        const relIDinv = `${eidMe.id}_${eidMo}`;
+        if (!(relID in rels) && !(relIDinv in rels)) {
+          const relation = {
+            id: relID,
+            source: eidMo,
+            target: eidMe.id,
+            direction: {},
+            order,
+          };
+          relation.direction[r.id] = r.is_reversible ? 0 : -1;
+          relation.reaction = new Set([r.id]);
+          rels[relID] = relation;
+        } else if (relID in rels) {
+          rels[relID].reaction.add(r.id);
+          rels[relID].direction[r.id] = r.is_reversible ? 0 : -1;
+        } else if (relIDinv in rels) {
+          rels[relIDinv].reaction.add(r.id);
+          rels[relIDinv].direction[r.id] = r.is_reversible ? 0 : -1;
+        }
+      }
+    }
+
+    // add relations between mets-mods components of the current reaction
+    for (const eidMo of Object.keys(mods)) {
+      for (const eidMe of [...r.products]) {
+        const relID = `${eidMo}_${eidMe.id}`;
+        const relIDinv = `${eidMe.id}_${eidMo}`;
+        if (!(relID in rels) && !(relIDinv in rels)) {
+          const relation = {
+            id: relID,
+            source: eidMo,
+            target: eidMe.id,
+            direction: {},
+            order,
+          };
+          relation.direction[r.id] = r.is_reversible ? 0 : 1;
+          relation.reaction = new Set([r.id]);
+          rels[relID] = relation;
+        } else if (relID in rels) {
+          rels[relID].reaction.add(r.id);
+          rels[relID].direction[r.id] = r.is_reversible ? 0 : 1;
+        } else if (relIDinv in rels) {
+          rels[relIDinv].reaction.add(r.id);
+          rels[relIDinv].direction[r.id] = r.is_reversible ? 0 : 1;
+        }
+      }
+    }
+
+    // add relations between mets components of the current reaction
+    for (const eidReactant of [...r.reactants, ...r.products]) {
+      for (const eidProduct of [...r.reactants, ...r.products]) {
+        if (eidReactant === eidProduct) {
+          continue; // eslint-disable-line no-continue
+        }
+        const relID = `${eidReactant.id}_${eidProduct.id}`;
+        const relIDinv = `${eidProduct.id}_${eidReactant.id}`;
+        if (!(relID in rels) && !(relIDinv in rels)) {
+          const relation = {
+            id: relID,
+            source: eidReactant.id,
+            target: eidProduct.id,
+            order,
+          };
+          relation.reaction = new Set([r.id]);
+          rels[relID] = relation;
+        } else if (relID in rels) {
+          rels[relID].reaction.add(r.id);
+        } else if (relIDinv in rels) {
+          rels[relIDinv].reaction.add(r.id);
+        }
+      }
+    }
+
+    // add relations with the main component
+    for (const eidMo of Object.keys(mods)) {
+      if (eidMo !== c.id) {
+        const relID = `${eidMo}_${c.id}`;
+        if (!(relID in rels)) {
+          const relation = {
+            id: `${eidMo}_${c.id}`,
+            target: eidMo,
+            source: c.id,
+            reaction: new Set([r.id]),
+            order,
+          };
+          rels[relID] = relation;
+        } else {
+          rels[relID].reaction.add(r.id);
+        }
+      }
     }
 
     for (const eidMe of Object.keys(mets)) {
-      const relation = {
-        id: `${eidMe}_${component.id}`,
-        target: eidMe,
-        source: component.id,
-        reaction: mets[eidMe].reaction,
-      };
-
-      rels[relation.id] = relation;
+      if (eidMe !== c.id) {
+        const relID = `${eidMe}_${c.id}`;
+        const relIDinv = `${c.id}_${eidMe}`;
+        if (relID in rels) {
+          rels[relID].reaction.add(r.id);
+        } else if (relIDinv in rels) {
+          rels[relIDinv].reaction.add(r.id);
+        }
+      }
     }
   }
-
-  return [elms, rels];
+  return [elms, rels, Array.from(compartmentSet), Array.from(subsystemSet)];
 }
