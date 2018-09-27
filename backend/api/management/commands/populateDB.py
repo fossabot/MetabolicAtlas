8d678c54eb097adc5a47d8791fc47f96de75bf73
pecholleyc <cholley@chalmers.se>
2018-09-27 20:29:55 +0200

    fix: allow to insert model without pmid/reference
    temporary remove insertion of authors, it seems wrong and does not allow multiple authors

--- backend/api/management/commands/populateDB.py
+++ backend/api/management/commands/populateDB.py
@@ -15,7 +15,7 @@ def insert_model_metadata(database, yaml_file, model_label, model_pmid):
         "metadata": {
             "id": {"type": "string"}, e.g. HMR3
             "label": {"type": "string"}, e.g. HMR 3.0
-            "full_name": {"type": "string"}, e.g. Human Metabolic Reaction version 3
+            "full_name": {"type": "string"}, e.g. Human Metabolic Reaction
             "description": {"type": "string"}, 
             "version": {
                 "type": "integer",
@@ -42,38 +42,43 @@ def insert_model_metadata(database, yaml_file, model_label, model_pmid):
 
     model = GEM.objects.using('gems').filter(name=gem.gemodelset.name, short_name=gem.label, database_name=database)
     if not model:
-        try:
-            publication = GEModelReference.objects.using('gems').get(pubmed=model_pmid) # 24419221 for hmr2
-            model = GEM(name=gem.gemodelset.name, short_name=gem.label, database_name=database, model=gem, publication=publication)
-        except GEModelReference.DoesNotExist:
+        if model_pmid:
+            try:
+                publication = GEModelReference.objects.using('gems').get(pubmed=model_pmid) # 24419221 for hmr2
+                model = GEM(name=gem.gemodelset.name, short_name=gem.label, database_name=database, model=gem, publication=publication)
+            except GEModelReference.DoesNotExist:
+                print ("Error: cannot find publication '%s' in gems database" % model_pmid)
+                exit(1)
+        else:
             model = GEM(name=gem.gemodelset.name, short_name=gem.label, database_name=database, model=gem)
         model.save(using='gems')
     else:
         model= model[0]
 
-    temp_author = {
-        'first_name': 'Adil',
-        'last_name': 'Mardinoglu',
-        'email': 'adilm@chalmers.se',
-        'organization': 'Chalmers University of Technology, Gothenburg',
-    }
+    # todo manager multiple authors
+    # temp_author = {
+    #     'first_name': 'Adil',
+    #     'last_name': 'Mardinoglu',
+    #     'email': 'adilm@chalmers.se',
+    #     'organization': 'Chalmers University of Technology, Gothenburg',
+    # }
 
-    a = Author.objects.using('gems').filter(given_name=temp_author['first_name'],
-                    family_name=temp_author['last_name'],
-                    email=temp_author['email'],
-                    organization=temp_author['organization'])
-    if not a:
-        author = Author(given_name=temp_author['first_name'],
-                    family_name=temp_author['last_name'],
-                    email=temp_author['email'],
-                    organization=temp_author['organization'])
-        author.save(using='gems')
+    # a = Author.objects.using('gems').filter(given_name=temp_author['first_name'],
+    #                 family_name=temp_author['last_name'],
+    #                 email=temp_author['email'],
+    #                 organization=temp_author['organization'])
+    # if not a:
+    #     author = Author(given_name=temp_author['first_name'],
+    #                 family_name=temp_author['last_name'],
+    #                 email=temp_author['email'],
+    #                 organization=temp_author['organization'])
+    #     author.save(using='gems')
 
-        ma = GEMAuthor(model=model, author=author)
-        ma.save(using='gems')
+    #     ma = GEMAuthor(model=model, author=author)
+    #     ma.save(using='gems')
 
 
-def populate_database(database, yaml_file, model_label, model_pmid, delete=False, metadata_only=False):
+def populate_database(database, yaml_file, model_label, model_pmid=None, delete=False, metadata_only=False):
 
     # Instert the model in the 
     insert_model_metadata(database, yaml_file, model_label, model_pmid)
@@ -108,11 +113,11 @@ class Command(BaseCommand):
         parser.add_argument('database', type=str)
         parser.add_argument('yaml file', type=str)
         parser.add_argument('model label', type=str)
-        parser.add_argument('model pmid', type=str)
+        parser.add_argument('--model-pmid', action='store', default=None, dest='model_pmid')
         parser.add_argument('--delete', action='store_true', dest='delete', default=False)
         parser.add_argument('--metadata-only', action='store_true', dest='metadata_only')
 
     def handle(self, *args, **options):
-        populate_database(options['database'], options['yaml file'],  options['model label'], options['model pmid'], delete=options['delete'], metadata_only=options['metadata_only'])
+        populate_database(options['database'], options['yaml file'],  options['model label'], model_pmid=options['model_pmid'], delete=options['delete'], metadata_only=options['metadata_only'])
 
 
