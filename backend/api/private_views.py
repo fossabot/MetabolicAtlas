fbe04d9a5891bb20a2f5d4bc2a8047427ae368f3
Mihail Anton <mihail.anton@chalmers.se>
2018-12-18 15:20:17 +0100

    fix: not return empty

--- backend/api/private_views.py
+++ backend/api/private_views.py
@@ -333,11 +333,8 @@ def get_db_json(request, model, component_name_id=None, ctype=None, dup_meta=Fal
 @api_view()
 def HPA_all_enzymes(request):
     model = "hmr2"
-    try:
-        result = APImodels.SubsystemEnzyme.objects.using(model).values_list('rc__id', 'subsystem__name','subsystem__name_id')
-        return JSONResponse(result)
-    except:
-        return JSONResponse({})
+    result = APImodels.SubsystemEnzyme.objects.using(model).values_list('rc__id', 'subsystem__name','subsystem__name_id')
+    return JSONResponse(result)
 
 @api_view()
 def HPA_enzyme_info(request, ensembl_id): # ENSG00000110921
@@ -347,7 +344,7 @@ def HPA_enzyme_info(request, ensembl_id): # ENSG00000110921
         res = APImodels.ReactionComponent.objects.using(model).get(id=ensembl_id)
         rcid = res.id
     except:
-        return JSONResponse({})
+        return HttpResponse(status=404)
 
     subs = APImodels.Subsystem.objects.using(model).filter(
             Q(id__in=APImodels.SubsystemEnzyme.objects.using(model).filter(rc_id=rcid).values('subsystem_id')) &
@@ -378,7 +375,7 @@ def HPA_enzyme_info(request, ensembl_id): # ENSG00000110921
         'enzyme_url': "https://icsb.chalmers.se/explore/gem-browser/%s/enzyme/%s" % (model, ensembl_id),
         'subsystems': subsystems,
         'doc': 'A subsystem can contain the same chemical metabolite that comes from different compartments.',
-    } if ensembl_id else None
+    }
 
     return JSONResponse(result)
 
