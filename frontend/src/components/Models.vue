c00d261be072937ff6dbc4a3eb234a691e1eebb2
Mihail Anton <mihail.anton@chalmers.se>
2019-02-13 10:24:34 +0100

    refactor: added cards, using vue good table MA-396

--- frontend/src/components/Models.vue
+++ frontend/src/components/Models.vue
@@ -4,46 +4,42 @@
       <div v-if="errorMessage">
         {{ errorMessage }}
       </div>
-      <div v-else id="models">
-        <span class="title">Genome-Scale Metabolic Models</span>
-        <br>
-        <div class="container">
-          <loader v-show="showLoader"></loader>
-          <div v-if="sortedGEMS.length != 0">
-            <div class="columns">
-              <div class="column">
-                <span id="show-m-but" class="button is-light" :class="{'is-active': showMaintained }"
-                  @click="toggleMaintainedModels">
-                  Maintained models only
-                </span>
-              </div>
-            </div>
-            <div class="columns">
-              <div class="column is-full">
-                <table class="table is-bordered is-striped is-narrow is-fullwidth" v-if="filteredOldGEMS.length != 0">
-                  <thead>
-                    <tr style="background: #F8F4F4">
-                      <th v-for="f in fields" v-html="f.display" :width="f.width ? f.width : ''"
-                        @click="sortBy(f.name)">
-                        </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr class="m-row" v-for="gem in filteredOldGEMS"
-                      @click="getModel(gem.id)">
-                      <td v-for="i in fields.length">
-                        {{ gem[fields[i-1].name] }}
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
+      <div v-else>
+        <span class="title">Integrated Genome-Scale Metabolic Models</span><br><br>
+        <span class="is-size-5">
+          These models are integrated into the Metabolic Atlas database - the models can be explored via {{ messages.gemBrowserName }}, {{ messages.mapViewerName }} and {{ messages.interPartName }}.
+        </span><br><br>
+        <div id="integrated" class="columns is-multiline is-variable is-6">
+          <div class="column is-6" v-for="model in models">
+            <div class="card is-size-5">
+            <header class="card-header has-background-primary">
+              <p class="card-content has-text-weight-bold has-text-white">{{ model.name }}</p>
+            </header>
+            <div class="card-content">
+              <div class="content">
+                Authors: {{ model.authors }}<br>
+                Year: {{ model.year }}<br>
+                # enzymes: {{ model.enzyme_count }}<br>
+                # metabolite: {{ model.metabolite_count }}<br>
+                # reactions: {{ model.reaction_count }}<br>
               </div>
             </div>
           </div>
-          <div v-else>
-            <span v-if="!showLoader">No models available</span>
           </div>
         </div>
+        <span class="title">Genome-Scale Metabolic Models</span><br><br>
+        <loader v-show="showLoader"></loader>
+        <div v-if="GEMS.length != 0">
+          <vue-good-table
+            :columns="columns" :rows="GEMS" :lineNumbers="true"
+            :sort-options="{ enabled: true }" styleClass="vgt-table striped bordered" :paginationOptions="tablePaginationOpts"
+            @on-row-click="getModel">
+          </vue-good-table>
+        </div>
+        <div v-else>
+          <span v-if="!showLoader">No models available</span>
+        </div>
+        <br>
         <div class="modal" v-bind:class="{ 'is-active': showModelTable }">
           <div class="modal-background" @click="showModelTable = false"></div>
           <div class="modal-content">
@@ -86,7 +82,7 @@
               </table>
             </div>
           </div>
-          <button class="modal-close" @click="showModelTable = false"></button>
+          <button class="modal-close is-large" @click="showModelTable = false"></button>
         </div>
       </div>
     </div>
@@ -97,8 +93,9 @@
 <script>
 import axios from 'axios';
 import $ from 'jquery';
+import { VueGoodTable } from 'vue-good-table';
+import 'vue-good-table/dist/vue-good-table.css';
 import Loader from 'components/Loader';
-import { default as compare } from '../helpers/compare';
 import { default as EventBus } from '../event-bus';
 import { default as messages } from '../helpers/messages';
 
@@ -106,20 +103,92 @@ export default {
   name: 'gems',
   components: {
     Loader,
+    VueGoodTable,
   },
   data() {
     return {
-      fields: [
-        { name: 'set_name', display: 'Set' },
-        { name: 'organism', display: 'Organism' },
-        { name: 'label', display: 'Label' },
-        // { name: 'organ_system', display: 'System' },
-        { name: 'condition', display: 'Condition' },
-        { name: 'tissue', display: 'Tissue/Cell&nbsp;type' },
-        { name: 'reaction_count', display: '#&nbsp;reactions', width: 100 },
-        { name: 'metabolite_count', display: '#&nbsp;metabolites', width: 100 },
-        { name: 'enzyme_count', display: '#&nbsp;enzymes', width: 100 },
-        { name: 'year', display: 'Year', width: 70 },
+      columns: [
+        {
+          label: 'Set',
+          field: 'set_name',
+          filterOptions: {
+            enabled: true,
+            // filterDropdownItems: [],
+          },
+          sortable: true,
+        }, {
+          label: 'Organism',
+          field: 'organism',
+          filterOptions: {
+            enabled: true,
+          },
+          sortable: true,
+          html: true,
+        }, {
+          label: 'Label',
+          field: 'label',
+          filterOptions: {
+            enabled: true,
+          },
+          sortable: true,
+        }, {
+          label: 'System',
+          field: 'organ_system',
+          filterOptions: {
+            enabled: true,
+          },
+          sortable: true,
+        }, {
+          label: 'Condition',
+          field: 'condition',
+          filterOptions: {
+            enabled: true,
+          },
+          sortable: true,
+        }, {
+          label: 'Tissue/Cell type',
+          field: 'tissue',
+          filterOptions: {
+            enabled: true,
+          },
+          sortable: true,
+        }, {
+          label: '# reactions',
+          field: 'reaction_count',
+          filterOptions: {
+            enabled: true,
+          },
+          sortable: true,
+        }, {
+          label: '# metabolites',
+          field: 'metabolite_count',
+          filterOptions: {
+            enabled: true,
+          },
+          sortable: true,
+        }, {
+          label: '# enzymes',
+          field: 'enzyme_count',
+          filterOptions: {
+            enabled: true,
+          },
+          sortable: true,
+        }, {
+          label: 'Year',
+          field: 'year',
+          filterOptions: {
+            enabled: true,
+          },
+          sortable: true,
+        }, {
+          label: 'Maintained',
+          field: 'maintained',
+          filterOptions: {
+            enabled: true,
+            filterDropdownItems: ['Yes', 'No'],
+          },
+          sortable: true,
+        },
       ],
       model_fields: [
         { name: 'organism', display: 'Organism' },
@@ -136,20 +205,25 @@ export default {
         { name: 'maintained', display: 'Maintained' },
       ],
       selectedModel: {},
-      sortAsc: true,
       errorMessage: '',
       GEMS: [],
-      sortedGEMS: [],
-      showMaintained: false,
       showModelTable: false,
       showLoader: false,
+      tablePaginationOpts: {
+        enabled: true,
+        perPage: 50,
+        position: 'both',
+        perPageDropdown: [25, 50, 100, 200],
+        dropdownAllowAll: false,
+        setCurrentPage: 1,
+        nextLabel: 'next',
+        prevLabel: 'prev',
+        rowsPerPageLabel: 'Rows per page',
+        ofLabel: 'of',
+      },
+      messages,
+      models: [],
     };
-  },
-  computed: {
-    filteredOldGEMS: function f() {
-      return this.showMaintained ?
-       this.sortedGEMS.filter(el => el.maintained) : this.sortedGEMS;
-    },
   },
   created() {
     EventBus.$on('viewGem', (modelID) => {
@@ -157,10 +231,26 @@ export default {
     });
   },
   beforeMount() {
+    this.getModelList();
     this.getModels();
   },
   methods: {
-    getModel(id) {
+    getModelList() {
+      // get models list
+      axios.get('models/')
+        .then((response) => {
+          const models = {};
+          for (const model of response.data) {
+            models[model.database_name] = model;
+          }
+          this.models = models;
+        })
+        .catch(() => {
+          this.errorMessage = messages.unknownError;
+        });
+    },
+    getModel(params) {
+      const id = params.row.id;
       axios.get(`gems/${id}`)
       .then((response) => {
         let model = response.data;
@@ -208,10 +298,9 @@ export default {
             gemex.tissue = gemex.cell_type;
           }
           delete gemex.cell_type;
+          gemex.maintained = gem.maintained ? 'Yes' : 'No';
           this.GEMS.push(gemex);
         }
-        this.sortedGEMS = this.GEMS.sort(
-          compare('set_name', null, 'asc'));
         this.errorMessage = '';
         this.showLoader = false;
       })
@@ -226,16 +315,6 @@ export default {
       }
       return ` <i>${this.selectedModel.organism}</i>: ${this.selectedModel.set_name} - ${this.selectedModel.tissue}`;
     },
-    sortBy(field) {
-      const gemsList = Array.prototype.slice.call(
-      this.sortedGEMS); // Do not mutate original elms;
-      this.sortedGEMS = gemsList.sort(
-        compare(field, null, this.sortAsc ? 'asc' : 'desc'));
-      this.sortAsc = !this.sortAsc;
-    },
-    toggleMaintainedModels() {
-      this.showMaintained = !this.showMaintained;
-    },
     getReferenceLink(ref) {
       const title = ref.title.replace(/&lt;/g, '<').replace(/&gt;/g, '>');
       if (ref.pubmed) {
@@ -249,48 +328,9 @@ export default {
 </script>
 
 <style lang="scss">
-
-#models {
-  .title {
-    display: block;
-    margin-bottom: 1rem;
-  }
-
-  th {
-    cursor: pointer;
-  }
-
-  #show-m-but {
-    margin-bottom: 1rem;
-    &.is-active {
-      border: 1px solid #505050;
-    }
-  }
-
-  .dsc, .name {
-    height: 75px;
-    line-height: 75px;
-
-    span {
-      display: inline-block;
-      vertical-align: middle;
-      line-height: normal;
-    }
-  }
-
-  .m-row {
-    cursor: pointer;
-  }
-
-  .name {
-    font-size: 1.5rem;
-  }
-
-  .modal-content {
-    width: 1024px;
-    padding: 1rem;
-    background: white;
+#integrated {
+  .card {
+    height: 100%;
   }
 }
-
 </style>
