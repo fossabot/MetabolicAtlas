cdb9a8a223a9924127070777f2e93e8f3fbd6260
Mihail Anton <mihail.anton@chalmers.se>
2019-07-10 17:12:23 +0200

    feat: bundle analyzer

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,3 +1,12 @@
 // vue.config.js
 module.exports = {
+  productionSourceMap: false,
+  pluginOptions: {
+    webpackBundleAnalyzer: {
+      analyzerMode: process.env.NODE_ENV === 'development' ? 'server' : 'disabled',
+      analyzerHost: 'frontend',
+      analyzerPort: 81,
+      openAnalyzer: false
+    }
+  }
 }
