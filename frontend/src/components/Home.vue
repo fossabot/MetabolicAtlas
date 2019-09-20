6abad350030d91684b5ae2ecbbe27f3a78127d4e
Mihail Anton <mihail.anton@chalmers.se>
2019-09-20 16:50:15 +0200

    feat: homepage mobile layout MA-623

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -19,13 +19,13 @@
     </section>
     <section class="section">
       <div id="home" class="container">
-        <div class="columns">
+        <div class="columns is-hidden-touch">
           <div class="column">
             <div class="card">
-              <div id="mobileMenu" class="columns is-mobile is-multiline margin-fix">
-                <div class="column is-narrow is-full-mobile has-background-primary has-text-weight-bold is-paddingless">
+              <div class="columns is-mobile is-multiline margin-fix">
+                <div class="column is-narrow has-background-primary has-text-weight-bold is-paddingless">
                   <aside class="menu">
-                    <ul class="menu-list is-size-5 is-unselectable">
+                    <ul id="menu-desktop" class="is-size-5 is-unselectable">
                       <li v-for="menuItem in menu" :key="menuItem.title">
                         <a :class="[ {'is-active' : menuItem.title === currentMenu.title}]"
                            @click="selectMenu(menuItem)">{{ menuItem.title }}</a>
@@ -33,10 +33,10 @@
                     </ul>
                   </aside>
                 </div>
-                <div class="column is-full-mobile more-padding">
+                <div class="column more-padding">
                   <p class="is-size-5 has-text-justified" v-html="currentMenu.text"></p>
                 </div>
-                <div class="column is-full-mobile more-padding is-v-aligned">
+                <div class="column more-padding is-v-aligned">
                   <router-link :to="currentMenu.url">
                     <div class="card" :class="{ 'card-selectable': currentMenu.cardLink}">
                       <img :src="currentMenu.img" :alt="currentMenu.title" />
@@ -47,6 +47,46 @@
                       </template>
                     </div>
                   </router-link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="columns is-hidden-desktop">
+          <div class="column">
+            <div class="card is-size-5">
+              <header class="card-header has-background-primary">
+                <div class="card-header-title is-centered">
+                  <div class="tabs is-centered">
+                    <ul id="menu-mobile">
+                      <li v-for="menuItem in menu" :key="menuItem.title">
+                        <a :class="[ {'is-active' : menuItem.title === currentMenu.title}]"
+                           @click="selectMenu(menuItem)">
+                           <i :class="`fa fa-${menuItem.icon} fa-lg`"></i>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </header>
+              <div class="card-content has-text-justified">
+                <p class="title has-text-centered">{{ currentMenu.title }}</p>
+                <div class="columns">
+                  <div class="colum is-half-tablet is-full-mobile">
+                    <p class="has-text-justified" v-html="currentMenu.text"></p>
+                  </div>
+                  <div class="column is-half-tablet is-full-mobile more-padding is-v-aligned">
+                    <router-link :to="currentMenu.url">
+                      <div class="card" :class="{ 'card-selectable': currentMenu.cardLink}">
+                        <img :src="currentMenu.img" :alt="currentMenu.title" />
+                        <template v-if="currentMenu.cardLink">
+                          <footer class="card-footer has-text-centered has-background-primary-lighter">
+                            <a class="card-footer-item is-size-5 has-text-weight-bold">{{ currentMenu.cardLink }}</a>
+                          </footer>
+                        </template>
+                      </div>
+                    </router-link>
+                  </div>
                 </div>
               </div>
             </div>
@@ -84,44 +124,53 @@ export default {
           text: '<b>Metabolic Atlas</b> integrates open source genome-scale metabolic models (GEMs) of human and yeast for easy browsing and analysis. It also contains many more genome scale metabolic models constructed by our organization.<br><br>Detailed biochemical information is provided for individual model components, such as reactions, metabolites, and genes. These components are also associated with standard identifiers, facilitating integration with external databases, such as the Human Protein Atlas. <br><br><i>Article under consideration</i>',
           img: require('../assets/explorer.jpg'),
           cardLink: 'Explore',
-          url: { name: 'explorerRoot' } },
+          url: { name: 'explorerRoot' },
+          icon: 'archive' },
         { title: messages.gemBrowserName,
           text: 'The <b>GEM Browser</b> enables powerful query and exploration of model content in tabular format.<br><br>A wide range of attributes, including reaction equations, metabolite formulas, gene rules and subsystem contents, are presented as a detailed network of individual model components. They are highly interconnected and rationally associated to easily navigate and switch between them.<br><br>Visit the documentation to learn about the different functionalities provided by the GEM Browser.',
           img: require('../assets/gemBrowser.jpg'),
           cardLink: 'Explore Human1 on the GEM Browser',
-          url: { name: 'browserRoot', params: { model: 'human1' } } },
-        { title: messages.interPartName,
-          text: `The <b>Interaction Partners</b> graph shows connectivity between metabolites and genes based on their associated reactions.<br><br>The graph is dynamically generated and is customizable. One can interact with a restricted part of the metabolic network, or further expand the interaction partners of any element already on the graph. Moreover, RNA expression data from the Human Protein Atlas can be overlaid onto the graph. <br><br>This feature is available only for metabolites and genes, and is accessible via the <b>${messages.gemBrowserName}</b>.`,
-          img: require('../assets/interaction.png'),
-          cardLink: 'View glyoxalate[p] in Interaction Partners',
-          url: { path: '/explore/gem-browser/human1/interaction/m02007p' } },
+          url: { name: 'browserRoot', params: { model: 'human1' } },
+          icon: 'database' },
         { title: messages.mapViewerName,
           text: 'For easy visualization, <b>Metabolic Atlas</b> handles both 2D and 3D maps. For each of the integrated models, the website automatically generates 3D graphs at both compartment and subsystem level.<br><br>Both compartment and subsystem 2D maps of the Human-GEM have been created by the contributors to Human-GEM and are manually curated. On these maps, one can search for reactions, metabolites or genes. Moreover, RNA expression data from Human Protein Atlas can be overlaid.<br><br>By clicking on an element on the map, more information of that element will be shown on the left sidebar. From there, one can navigate back to the <b>GEM Browser</b> for detailed information.',
           img: require('../assets/mapViewer.jpg'),
           cardLink: 'Explore Human1 on the Map Viewer',
-          url: { name: 'viewer', params: { model: 'human1' } } },
+          url: { name: 'viewer', params: { model: 'human1' } },
+          icon: 'map-o' },
+        { title: messages.interPartName,
+          text: `The <b>Interaction Partners</b> graph shows connectivity between metabolites and genes based on their associated reactions.<br><br>The graph is dynamically generated and is customizable. One can interact with a restricted part of the metabolic network, or further expand the interaction partners of any element already on the graph. Moreover, RNA expression data from the Human Protein Atlas can be overlaid onto the graph. <br><br>This feature is available only for metabolites and genes, and is accessible via the <b>${messages.gemBrowserName}</b>.`,
+          img: require('../assets/interaction.png'),
+          cardLink: 'View glyoxalate[p] in Interaction Partners',
+          url: { path: '/explore/gem-browser/human1/interaction/m02007p' },
+          icon: 'connectdevelop' },
         { title: 'Search',
           text: 'The menu bar contains a shortcut to the <b>Global search</b> function, which enables users to easily search cellular components across all the integrated models. Further filtering is also available, based on result type (e.g. metabolite) and its parameters (e.g. compartment).<br><br>For retrieving larger subsets of the model, we recommend experienced users to use our the API. Alternatively, models can be downloaded from the <b>GEM Repository</b> page or from their original repository on GitHub.<br><br>',
           img: require('../assets/search.jpg'),
           cardLink: 'Global search',
-          url: { name: 'search', query: { term: '' } } },
-        { title: 'Export',
-          text: '<b>Metabolic Atlas</b> provides open access to the models and associated annotations. Most of the data provided on <b>Metabolic Atlas</b> is convenient to export, look for export buttons.<br><br>For the ones interested in extracting data in JSON format, we have documented our API. When using images or files obtained from our website, use the following reference:<br><br><i>Article under consideration</i>',
-          img: require('../assets/export.jpg'),
-          url: '' },
+          url: { name: 'search', query: { term: '' } },
+          icon: 'search' },
         { title: 'Analyze',
           text: 'Gene expression data from the Human Protein Atlas can be viewed in the 2D and 3D maps and Interaction Partners. User data can also be overlaid onto the maps, with the option of comparing datasets, for example against normal tissue.<br><br>Additional types of omics integrations are under development.',
           img: require('../assets/analyze.jpg'),
-          url: '' },
+          url: '',
+          icon: 'eye' },
         { title: 'GEM Repository',
           text: 'Over 350 GEMs can be downloaded from the browser or directly from the <b>Metabolic Atlas FTP server</b>. The tabular view enables customized selection.<br><br>Clicking on each of the models brings up more information about the model, including a text description and, if available, references. For support, the original authors should be contacted.',
           img: require('../assets/gems.jpg'),
           cardLink: 'GEM Repository',
-          url: { name: 'gems' } },
+          url: { name: 'gems' },
+          icon: 'files-o' },
+        { title: 'Export',
+          text: '<b>Metabolic Atlas</b> provides open access to the models and associated annotations. Most of the data provided on <b>Metabolic Atlas</b> is convenient to export, look for export buttons.<br><br>For the ones interested in extracting data in JSON format, we have documented our API. When using images or files obtained from our website, use the following reference:<br><br><i>Article under consideration</i>',
+          img: require('../assets/export.jpg'),
+          url: '',
+          icon: 'download' },
         { title: 'Resources',
           text: 'Working with metabolic models requires a set of tools and external databases, which we have collected together for one-click access.<br><br>Additionally, Metabolic Atlas is open to further integrations.',
           img: require('../assets/resources.jpg'),
-          url: { name: 'resources' } },
+          url: { name: 'resources' },
+          icon: 'gears' },
       ],
       currentMenu: {},
       cards: [
@@ -158,12 +207,7 @@ export default {
 <style lang="scss">
 
 #home {
-  #mobileMenu {
-    .column:first-child {
-      outline: 1px solid $primary;
-    }
-  }
-  .menu-list li {
+  #menu-desktop li {
     &:first-child {
       margin-top: 1em;
     }
@@ -186,6 +230,18 @@ export default {
       border-radius: 0;
     }
   }
+  #menu-mobile li {
+    a {
+      color: $white-bis;
+    }
+    a:hover {
+      background-color: $primary-light;
+    }
+    .is-active {
+      color: $black;
+      background-color: $white;
+    }
+  }
   .margin-fix {
     margin-top: 1rem;
     margin-bottom: 1rem;
@@ -204,9 +260,6 @@ export default {
     display: flex;
     align-items: center;
     justify-content: center;
-  }
-  #mobileMenu {
-    margin-right: 0;
   }
 }
 
