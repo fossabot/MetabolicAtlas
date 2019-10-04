efea7161ed4d2f38e0389b3e9da2455b683e03a6
pecholleyc <cholley@chalmers.se>
2019-10-04 15:39:46 +0200

    feat: add postgres contrib app to be able to install postgres extensions
    the extension fuzzystrmatch will be required and has been added in the migration file 'initial' (not tracked) as such:
    
    from django.contrib.postgres.operations import CreateExtension
    ...
        operations = [
            CreateExtension(name='fuzzystrmatch'),
            ...

--- backend/metabolicatlas/settings.py
+++ backend/metabolicatlas/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = [
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
     'rest_framework',
     'rest_framework_swagger',
     'api',
