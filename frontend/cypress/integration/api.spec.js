fa79cfab224ae7112bf0ce26196ee2c5dde591aa
Mihail Anton <mihail.anton@chalmers.se>
2020-02-04 14:19:07 +0100

    feat: testing API MA-713

--- frontend/cypress/integration/api.spec.js
+++ frontend/cypress/integration/api.spec.js
@@ -1,51 +1,61 @@
 /// <reference types="Cypress" />
 context('API endpoints', () => {
 
-  describe('SingleCompoEndPoints', () => {
-    const APIbaseURL = '/api'
-    const model = 'human1'
-    urls = [
+  const APIbaseURL = '/api'
+  const model = 'human1'
+
+  describe('SingleComponentEndPoints', () => {
+    it('Return single component JSON for Human1', () => {
+      const urls = [
         [`${APIbaseURL}/${model}/reaction/HMR_1547`, 'api.reaction.HMR_1547'],
         [`${APIbaseURL}/${model}/metabolite/m02439c`, 'api.metabolite.m02439c'],
-        [`${APIbaseURL}/${model}/gene/ENSG00000106636`, 'api.gene.ENSG00000106636'],
         [`${APIbaseURL}/${model}/compartment/nucleus`, 'api.compartment.nucleus'],
-        [`${APIbaseURL}/${model}/subsystem/pyruvate_metabolism`, 'api.subsystem.pyruvate'],
-        [`${APIbaseURL}/${model}/hpa/gene/ENSG00000106636`, 'api.hpagene.ENSG00000106636'],
-    ]
+        [`${APIbaseURL}/hpa/gene/ENSG00000106636`, 'api.hpagene.ENSG00000106636'],
+        [`${APIbaseURL}/${model}/gene/ENSG00000140795`, 'api.gene.ENSG00000140795'],
+        [`${APIbaseURL}/${model}/subsystem/coa_catabolism`, 'api.subsystem.coa_catabolism'],
+      ]
 
-    cy.server()
-    urls.forEach((url, jsonFile) => {
-        cy.route({method: 'GET', url: url}).as('apiCall')
-        cy.wait('@apiCall').then(function(xhr) {
-          const response = xhr.responseBody
-          cy.fixture(`api/${jsonFile}`).as('jsonData')
-          expect(response).to.eql('@jsonData')
+      urls.forEach(endpoint => {
+        const url = endpoint[0];
+        const jsonFile = endpoint[1];
+        cy.request(url)
+          .then(({headers, status, body}) => {
+            expect(headers['content-type']).to.include('application/json')
+            expect(status).to.eql(200)
+            console.log(body)
+            cy.fixture(`api/${jsonFile}`).then((jsonData) => {
+              cy.log(JSON.stringify(jsonData))
+              expect(body).to.eql(jsonData)
+            })
+          })
         })
-    }
-  })
+      })
+    })
 
-  describe('MultiCompoEndPoints', () => {
-    const APIbaseURL = '/api'
-    const model = 'human1'
-    urls = [
+  describe('MultiComponentEndPoints', () => {
+    it('Return single component JSON for Human1', () => {
+      const urls = [
+        [`${APIbaseURL}/${model}/subsystems/`, 'api.subsystems'],
         [`${APIbaseURL}/${model}/reactions/`, 'api.reactions'],
         [`${APIbaseURL}/${model}/metabolites/`, 'api.metabolites'],
         [`${APIbaseURL}/${model}/genes/`, 'api.genes'],
         [`${APIbaseURL}/${model}/compartments/`, 'api.compartments'],
-        [`${APIbaseURL}/${model}/subsystems/`, 'api.subsystems'],
-        [`${APIbaseURL}/${model}/hpa/genes/`, 'api.hapgenes'],
-    ]
+        [`${APIbaseURL}/hpa/genes/`, 'api.hpagenes'],
+      ]
 
-    cy.server()
-    urls.forEach((url, jsonFile) => {
-        cy.route({method: 'GET', url: url}).as('apiCall')
-        cy.wait('@apiCall').then(function(xhr) {
-          const response = xhr.responseBody
-          cy.fixture(`api/${jsonFile}`).as('jsonData')
-          expect(response.length).to.be.at.least(2)
-          expect(response[0]).to.eql('@jsonData')
+      urls.forEach(endpoint => {
+        const url = endpoint[0];
+        const jsonFile = endpoint[1];
+        cy.request(url)
+          .then(({headers, status, body}) => {
+            expect(headers['content-type']).to.include('application/json')
+            expect(status).to.eql(200)
+            cy.fixture(`api/${jsonFile}`).then((jsonData) => {
+              expect(body[0]).to.eql(jsonData)
+            })
+          })
         })
-    }
-  })
+      })
+    })
 
 })
