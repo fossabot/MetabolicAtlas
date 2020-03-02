4c5aa75159f98368d9a465a72c42deaa81b13191
e0 <shan.huang@me.com>
2020-03-02 13:24:42 +0100

    fix: get reaction query

--- frontend/src/neo4j/queries/reaction.js
+++ frontend/src/neo4j/queries/reaction.js
@@ -4,8 +4,9 @@ import handleSingleResponse from '../responseHandlers/single';
 const getReaction = async (id) => {
   const statement = `
 MATCH (r:Reaction)-[:V1]-(rs:ReactionState)
-MATCH (r)-[:V1]-(c:Compartment)-[:V1]-(cs:CompartmentState)
+MATCH (r)-[:V1]-(:Metabolite)-[:V1]-(c:Compartment)-[:V1]-(cs:CompartmentState)
 MATCH (r)-[:V1]-(s:Subsystem)-[:V1]-(ss:SubsystemState)
+MATCH (r)-[:V1]-(g:Gene)-[:V1]-(gs:GeneState)
 MATCH (r)-[:V1]-(e:ExternalDb)
 MATCH (r)-[:V1]-(p:PubmedReference)
 WHERE r.id="${id}"
@@ -13,13 +14,12 @@ RETURN
   r.id as id,
   rs.reversible as reversible,
   rs.lowerBound as lowerBound,
-  rs.upperBound as uppwerBound,
+  rs.upperBound as upperBound,
   rs.geneRule as geneRule,
   rs.ec as ec,
-  c.id as compartmentId,
-  cs.name as compartmentName,
-  s.id as subsystemId,
-  ss.name as subsystemName,
+  COLLECT(DISTINCT({id: c.id, name: cs.name})) as compartments,
+  COLLECT(DISTINCT({id: s.id, name: ss.name})) as subsystems,
+  COLLECT(DISTINCT({id: g.id, name: gs.name})) as genes,
   e.dbName as externalDbName,
   e.url as externalDbUrl,
   e.externalId as externalDbId,
