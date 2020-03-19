b76d77924b8be34155571ac6f5e1efbdf710ebda
Mihail Anton <mihail.anton@chalmers.se>
2020-03-19 15:25:10 +0100

    fix: use only id for gem-contact consistently

--- frontend/src/components/explorer/gemBrowser/Subsystem.vue
+++ frontend/src/components/explorer/gemBrowser/Subsystem.vue
@@ -68,7 +68,7 @@
       </div>
       <div class="column is-2-widescreen is-3-desktop is-half-tablet has-text-centered">
         <maps-available :id="sName" :model="model" :type="type" :element-i-d="''" />
-        <gem-contact :id="info.name" :model="model" :type="type" />
+        <gem-contact :id="sName" :model="model" :type="type" />
       </div>
     </div>
     <template v-if="!showLoader">
