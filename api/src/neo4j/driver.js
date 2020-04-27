898b09561ef6e2ca29625268f96fdc81b0fd9f3f
e0 <shan.huang@me.com>
2020-04-27 11:54:18 +0200

    fix: refer to neo4j host by name instead of ip

--- api/src/neo4j/driver.js
+++ api/src/neo4j/driver.js
@@ -1,7 +1,7 @@
 import neo4j from 'neo4j-driver'
 
 // TODO: put uri in an environment variable
-const uri = 'bolt://172.18.0.7'
+const uri = 'bolt://neo4j'
 const driver = neo4j.driver(uri)
 
 process.on('exit', async () => {
