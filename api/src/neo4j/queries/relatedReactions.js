9f638c11eade0b0792c8611301efaf629a912c02
e0 <shan.huang@me.com>
2020-05-04 17:54:58 +0200

    fix: update cypher query for retrieving related reactions for a reaction

--- api/src/neo4j/queries/relatedReactions.js
+++ api/src/neo4j/queries/relatedReactions.js
@@ -1,7 +1,6 @@
 import queryListResult from '../queryHandlers/list';
 
 const NODE_TYPES = {
-  reaction: 'Reaction',
   gene: 'Gene',
   metabolite: 'Metabolite',
   subsystem: 'Subsystem',
@@ -9,18 +8,37 @@ const NODE_TYPES = {
 };
 
 // TODO: add pagination and search
+const getRelatedReactionsForReaction = ({ id, version }) => {
+  const v = version;
+
+  const statement = `
+MATCH (rr:Reaction)-[rcmE:V${v}]-(rcm:CompartmentalizedMetabolite)-[:V${v}]-(m:Metabolite)-[:V${v}]-(xcm:CompartmentalizedMetabolite)-[:V${v}]-(x:Reaction)-[:V${v}]-(:CompartmentalizedMetabolite)-[:V${v}]-(:Metabolite)-[:V${v}]-(:CompartmentalizedMetabolite)-[:V${v}]-(rr)
+WHERE x.id="${id}"
+MATCH (rr)-[:V${v}]-(rrs:ReactionState)
+MATCH (m)-[:V${v}]-(ms:MetaboliteState)
+OPTIONAL MATCH (rcm)-[:V${v}]-(c:Compartment)-[:V${v}]-(cs:CompartmentState)
+OPTIONAL MATCH (rr)-[:V${v}]-(s:Subsystem)-[:V${v}]-(ss:SubsystemState)
+OPTIONAL MATCH (rr)-[:V${v}]-(g:Gene)-[:V${v}]-(gs:GeneState)
+RETURN rrs {
+  .*,
+  id: rr.id,
+  compartments: COLLECT(DISTINCT(cs {id: c.id, .*})),
+  subsystems: COLLECT(DISTINCT(ss {id: s.id, .*})),
+  metabolites: COLLECT(DISTINCT(ms {id: m.id, stoichiometry: rcmE.stoichiometry, outgoing: startnode(rcmE)=m, .*})),
+  genes: COLLECT(DISTINCT(gs {id: g.id, .*}))
+} as reactions
+`;
+
+  console.log(statement);
+
+  return queryListResult(statement);
+};
+
 const getRelatedReactions = async ({ nodeType, id, version }) => {
   const v = version;
   let statement;
 
   switch (nodeType) {
-    case NODE_TYPES.reaction:
-      statement = `
-MATCH (x:Reaction)-[:V${v}]-(m:Metabolite)
-WHERE x.id="${id}"
-MATCH (x)-[:V${v}]-(m)-[metaboliteEdge:V${v}]-(related:Reaction)-[:V${v}]-(relatedS:ReactionState)
-MATCH (m)-[:V${v}]-(ms:MetaboliteState)`;
-      break;
     case NODE_TYPES.gene:
       statement = `
 MATCH (x:Gene)-[:V${v}]-(related:Reaction)-[metaboliteEdge:V${v}]-(m:Metabolite)
@@ -69,9 +87,6 @@ RETURN relatedS {
   return queryListResult(statement);
 };
 
-const getRelatedReactionsForReaction = ({ id, version }) => getRelatedReactions({
-  id, version, nodeType: NODE_TYPES.reaction,
-});
 
 const getRelatedReactionsForGene = ({ id, version }) => getRelatedReactions({
   id, version, nodeType: NODE_TYPES.gene,
