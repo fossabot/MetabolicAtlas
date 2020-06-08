a3fed9e55e9fc68d4a33e566db7a045ebaaff0c5
e0 <shan.huang@me.com>
2020-06-08 16:01:19 +0200

    fix: related reactions

--- api/src/neo4j/queries/relatedReactions.js
+++ api/src/neo4j/queries/relatedReactions.js
@@ -1,6 +1,7 @@
 import queryListResult from '../queryHandlers/list';
 
 const NODE_TYPES = {
+  reaction: 'Reaction',
   gene: 'Gene',
   metabolite: 'Metabolite',
   subsystem: 'Subsystem',
@@ -8,95 +9,100 @@ const NODE_TYPES = {
 };
 
 // TODO: add pagination and search
-const getRelatedReactionsForReaction = ({ id, version }) => {
-  const v = version;
-
-  const statement = `
-MATCH (rrs:ReactionState)-[:V${v}]-(rr:Reaction)-[rcmE:V${v}]-(rcm:CompartmentalizedMetabolite)-[:V${v}]-(m:Metabolite)-[:V${v}]-(xcm:CompartmentalizedMetabolite)-[:V${v}]-(x:Reaction)-[:V${v}]-(:CompartmentalizedMetabolite)-[:V${v}]-(:Metabolite)-[:V${v}]-(:CompartmentalizedMetabolite)-[:V${v}]-(rr)
-WHERE x.id="${id}"
-MATCH (m)-[:V${v}]-(ms:MetaboliteState)
-OPTIONAL MATCH (rcm)-[:V${v}]-(c:Compartment)-[:V${v}]-(cs:CompartmentState)
-OPTIONAL MATCH (rr)-[:V${v}]-(s:Subsystem)-[:V${v}]-(ss:SubsystemState)
-OPTIONAL MATCH (rr)-[:V${v}]-(g:Gene)-[:V${v}]-(gs:GeneState)
-RETURN rrs {
-  .*,
-  id: rr.id,
-  compartments: COLLECT(DISTINCT(cs {id: c.id, .*})),
-  subsystems: COLLECT(DISTINCT(ss {id: s.id, .*})),
-  metabolites: COLLECT(DISTINCT(ms {id: rcm.id, stoichiometry: rcmE.stoichiometry, outgoing: startnode(rcmE)=rcm, .*})),
-  genes: COLLECT(DISTINCT(gs {id: g.id, .*}))
-} as reactions
-`;
-
-  return queryListResult(statement);
-};
-
 const getRelatedReactions = async ({ nodeType, id, version }) => {
   const v = version;
   let statement;
 
   switch (nodeType) {
+    case NODE_TYPES.reaction:
+      statement = `
+MATCH (r1:Reaction {id: '${id}'})-[cms1:V${v}]-(:CompartmentalizedMetabolite)-[:V${v}]-(m:Metabolite)
+WITH r1, count(cms1) as ccms1, collect(distinct(m)) as ms
+UNWIND ms as m
+MATCH (m)-[:V${v}]-(:CompartmentalizedMetabolite)-[cms2:V${v}]-(r:Reaction)
+WITH r1, r, count(cms2) as ccms2, ccms1
+WHERE ccms1 = ccms2
+MATCH (r)-[cms3:V${v}]-(:CompartmentalizedMetabolite)
+WITH ccms1, count(cms3) as ccms3, r1, r
+WHERE ccms1 = ccms3 and r1.id <> r.id`;
+      break;
     case NODE_TYPES.gene:
       statement = `
-MATCH (x:Gene)-[:V${v}]-(related:Reaction)-[metaboliteEdge:V${v}]-(cm:CompartmentalizedMetabolite)-[:V${v}]-(:Metabolite)-[:V${v}]-(ms:MetaboliteState)
-WHERE x.id="${id}"
-MATCH (related)-[:V${v}]-(relatedS:ReactionState)`;
+MATCH (:Gene {id: '${id}'})-[:V${v}]-(r:Reaction)`;
       break;
     case NODE_TYPES.metabolite:
       statement = `
-MATCH (x:CompartmentalizedMetabolite)-[:V${v}]-(related:Reaction)-[:V${v}]-(relatedS:ReactionState)
-WHERE x.id="${id}"
-MATCH (related)-[metaboliteEdge:V${v}]-(cm:CompartmentalizedMetabolite)-[:V${v}]-(:Metabolite)-[:V${v}]-(ms:MetaboliteState)`;
+MATCH (:CompartmentalizedMetabolite {id: '${id}'})-[:V${v}]-(r:Reaction)`;
       break;
     case NODE_TYPES.subsystem:
       statement = `
-MATCH (x:Subsystem)-[:V${v}]-(related:Reaction)-[metaboliteEdge:V${v}]-(cm:CompartmentalizedMetabolite)-[:V${v}]-(:Metabolite)-[:V${v}]-(ms:MetaboliteState)
-WHERE x.id="${id}"
-MATCH (related)-[:V${v}]-(relatedS:ReactionState)`;
+MATCH (:Subsystem {id: '${id}'})-[:V${v}]-(r:Reaction)`;
       break;
     case NODE_TYPES.compartment:
       statement = `
-MATCH (x:Compartment)-[:V${v}]-(cm:CompartmentalizedMetabolite)-[metaboliteEdge:V${v}]-(related:Reaction)-[:V${v}]-(relatedS:ReactionState)
-WHERE x.id="${id}"
-MATCH (cm)-[:V${v}]-(:Metabolite)-[:V${v}]-(ms:MetaboliteState)`;
+MATCH (:Compartment {id: '${id}'})-[:V${v}]-(:CompartmentalizedMetabolite)-[:V${v}]-(r:Reaction)`;
       break;
     default:
       throw new Error(`Unrecognized node type: ${nodeType}`);
   }
 
   statement += `
-OPTIONAL MATCH (cm)-[:V${v}]-(c:Compartment)-[:V${v}]-(cs:CompartmentState)
-OPTIONAL MATCH (related)-[:V${v}]-(s:Subsystem)-[:V${v}]-(ss:SubsystemState)
-OPTIONAL MATCH (related)-[:V${v}]-(g:Gene)-[:V${v}]-(gs:GeneState)
-RETURN relatedS {
-  id: related.id,
-  compartments: COLLECT(DISTINCT(cs {id: c.id, .*})),
-  subsystems: COLLECT(DISTINCT(ss {id: s.id, .*})),
-  metabolites: COLLECT(DISTINCT(ms {id: cm.id, stoichiometry: metaboliteEdge.stoichiometry, outgoing: startnode(metaboliteEdge)=cm, .*})),
-  genes: COLLECT(DISTINCT(gs {id: g.id, .*}))
-} as reactions
+WITH r.id as rid
+LIMIT 100
+CALL apoc.cypher.run("
+  MATCH (rs:ReactionState)-[:V${v}]-(r:Reaction {id: $rid})
+  RETURN rs { id: $rid, .* } as data
+ 
+  UNION
+ 
+  MATCH (r:Reaction {id: $rid})-[:V${v}]-(cm:CompartmentalizedMetabolite)
+  WITH DISTINCT cm
+  MATCH (cm)-[:V${v}]-(c:Compartment)-[:V${v}]-(cs:CompartmentState)
+  RETURN { id: $rid, compartments: COLLECT(DISTINCT(cs {id: c.id, .*})) } as data
+ 
+  UNION
+ 
+  MATCH (r:Reaction {id: $rid})-[:V${v}]-(s:Subsystem)-[:V${v}]-(ss:SubsystemState)
+  RETURN { id: $rid, subsystems: COLLECT(DISTINCT(ss {id: s.id, .*})) } as data
+ 
+  UNION
+ 
+  MATCH (r:Reaction {id: $rid})-[:V${v}]-(g:Gene)-[:V${v}]-(gs:GeneState)
+  RETURN { id: $rid, genes: COLLECT(DISTINCT(gs {id: g.id, .*})) } as data
+ 
+  UNION
+ 
+  MATCH (r:Reaction {id: $rid})-[cmE:V${v}]-(cm:CompartmentalizedMetabolite)-[:V${v}]-(:Metabolite)-[:V${v}]-(ms:MetaboliteState)
+  RETURN { id: $rid, metabolites: COLLECT(DISTINCT(ms {id: cm.id, stoichiometry: cmE.stoichiometry, outgoing: startnode(cmE)=cm, .*})) } as data
+", {rid:rid}) yield value
+RETURN apoc.map.mergeList(apoc.coll.flatten(
+    apoc.map.values(apoc.map.groupByMulti(COLLECT(value.data), "id"), [value.data.id])
+)) as reactions
 `;
-
 
   return queryListResult(statement);
 };
 
 
+const getRelatedReactionsForReaction = ({ id, version }) => getRelatedReactions({
+  id, version, nodeType: NODE_TYPES.reaction,
+  });
+
 const getRelatedReactionsForGene = ({ id, version }) => getRelatedReactions({
   id, version, nodeType: NODE_TYPES.gene,
-});
+  });
 
 const getRelatedReactionsForMetabolite = ({ id, version }) => getRelatedReactions({
   id, version, nodeType: NODE_TYPES.metabolite,
-});
+  });
 
 const getRelatedReactionsForSubsystem = ({ id, version }) => getRelatedReactions({
   id, version, nodeType: NODE_TYPES.subsystem,
-});
+  });
 
 const getRelatedReactionsForCompartment = ({ id, version }) => getRelatedReactions({
   id, version, nodeType: NODE_TYPES.compartment,
-});
+  });
 
 export {
   getRelatedReactionsForReaction,
