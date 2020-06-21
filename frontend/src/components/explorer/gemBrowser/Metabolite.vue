65836293fe7e22f1a0738c0144b51d27cc7f02e6
e0 <shan.huang@me.com>
2020-06-21 14:49:29 +0200

    fix: metabolite table

--- frontend/src/components/explorer/gemBrowser/Metabolite.vue
+++ frontend/src/components/explorer/gemBrowser/Metabolite.vue
@@ -54,14 +54,14 @@
                       <br v-if="i !== 0">
                       <!-- eslint-disable-next-line max-len -->
                       <router-link :to="{ name: 'browser', params: { model: model.database_name, type: 'metabolite', id: rm.id } }">
-                        {{ rm.id }}
+                        {{ rm.fullName }}
                       </router-link> in {{ rm.compartment.name }}
                     </span>
                   </td>
                 </tr>
               </table>
             </div>
-            <ExtIdTable :type="type" :external-dbs="metabolite.external_databases"></ExtIdTable>
+            <ExtIdTable :type="type" :external-dbs="metabolite.externalDbs"></ExtIdTable>
           </div>
           <div class="column is-2-widescreen is-3-desktop is-full-tablet has-text-centered">
             <router-link class="button is-info is-fullwidth is-outlined"
