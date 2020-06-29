abdcf0e007aec43eb43587003add1a0c148443dd
e0 <shan.huang@me.com>
2020-06-29 16:33:00 +0200

    fix: neo4j authentication

--- api/src/neo4j/driver.js
+++ api/src/neo4j/driver.js
@@ -1,7 +1,12 @@
 import neo4j from 'neo4j-driver';
 
+
 const uri = 'bolt://neo4j';
-const driver = neo4j.driver(uri, null, { disableLosslessIntegers: true });
+const driver = neo4j.driver(
+  uri,
+  neo4j.auth.basic(process.env.NEO4J_USERNAME, process.env.NEO4J_PASSWORD),
+  { disableLosslessIntegers: true }
+);
 // https://github.com/neo4j/neo4j-javascript-driver#enabling-native-numbers
 
 process.on('exit', async () => {
