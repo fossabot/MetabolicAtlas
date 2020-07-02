572c13544faadf0ddc600a4dc8029a775eb8ed8c
pecholleyc <cholley@chalmers.se>
2020-07-02 14:30:10 +0200

    refactor: load the json with all tissues + levels

--- frontend/src/api/humanProteinAtlas.js
+++ frontend/src/api/humanProteinAtlas.js
@@ -1,24 +1,10 @@
 import axios from 'axios';
 
-const baseURL = '/api/v1/';
+const baseURL = '/api/v2/';
 
-const fetchTissues = async (model) => {
-  const { data } = await axios({ url: `${model}/gene/hpa_tissue/`, baseURL });
+const fetchRnaLevels = async () => {
+  const { data } = await axios({ url: '/rna/all/', baseURL });
   return data;
 };
 
-const fetchRnaLevels = async (model, geneIds) => {
-  const { data } = await axios({
-    method: 'post',
-    url: `${model}/gene/hpa_rna_levels/`,
-    data: geneIds,
-  });
-  return data;
-};
-
-const fetchRnaLevelsForMap = async (model, mapType, dim, mapName) => {
-  const { data } = await axios({ url: `${model}/gene/hpa_rna_levels/${mapType}/${dim}/${mapName}`, baseURL });
-  return data;
-};
-
-export default { fetchTissues, fetchRnaLevels, fetchRnaLevelsForMap };
+export default { fetchRnaLevels };
